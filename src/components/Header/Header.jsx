88ee698717000b4fe9c97630ded3a913f8143976
Eric Rovell <ericrovell@gmail.com>
2020-01-08 22:30:26 +0300

    moved pages to src; test Header import

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,5 @@
+const Header = () => (
+  <h1>Numeral Base Bulbs 2</h1>
+);
+
+export default Header;
---
+++ src/pages/index.jsx
@@ -0,0 +1,12 @@
+import Header from "components/Header/Header";
+
+const HomePage = () => {  
+  return (
+    <main>
+      <Header />
+      <div>Welcome to Next.js!</div>
+    </main>
+  );
+};
+
+export default HomePage;
