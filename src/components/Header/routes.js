592617f2104b7e31200fd552748de976879c6373
Eric Rovell <ericrovell@gmail.com>
2020-01-08 22:38:37 +0300

    routes for Header, initial version

---
+++ src/components/Header/routes.js
@@ -0,0 +1,37 @@
+export default [
+  {
+    path: "/",
+    name: {
+      "EN": "Sandbox",
+      "RU": "Песочница",
+    },
+  },
+  {
+    path: "/",
+    name: {
+      "EN": "Game",
+      "RU": "Игра",
+    },
+  },
+  {
+    path: "/",
+    name: {
+      "EN": "Inctructions",
+      "RU": "Инструкции",
+    },
+  },
+  {
+    path: "/",
+    name: {
+      "EN": "Settings",
+      "RU": "Настройки",
+    },
+  },
+  {
+    path: "/",
+    name: {
+      "EN": "About",
+      "RU": "О приложении",
+    },
+  }, 
+];
