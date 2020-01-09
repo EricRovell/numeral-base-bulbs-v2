674583428e9d9994cb77a605573f9761c38e56d0
Eric Rovell <ericrovell@gmail.com>
2020-01-09 13:46:40 +0300

    initialState for app, wip

--- src/context/initialState.js
+++ src/context/initialState.js
@@ -1,4 +1,19 @@
 export default {
   lang: "EN",
   theme: "dark",
+
+  digits: [0, 0, 0, 0, 0, 0, 0, 0],
+  representation: "bulb",  // bulb | bulb-realistic | numeral | char
+
+  digitsMin: 0,
+  digitsMax: 16,
+  digitsDefault: 8,
+  
+  baseIn: 2,
+  baseOut: 10,
+  baseMin: 2,
+  baseMax: 16,
+
+  labelsUp: "index",    // index | value | power | hidden
+  labelsDown: "value",
 };
