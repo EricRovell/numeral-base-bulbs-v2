b6b31a4488a06492720d9088da38d4e3850efa4c
Eric Rovell <ericrovell@gmail.com>
2020-01-10 18:47:26 +0300

    style alias absolute import typo fix

--- next.config.js
+++ next.config.js
@@ -10,7 +10,7 @@ module.exports = withCSS({
 
   webpack: (config, options) => {
     config.resolve.alias["components"] = path.join(__dirname, "src/components");
-    config.resolve.alias["styles"] = path.join(__dirname, "src/styles");
+    config.resolve.alias["style"] = path.join(__dirname, "src/style");
     config.resolve.alias["pages"] = path.join(__dirname, "src/pages");
 
     return config;
