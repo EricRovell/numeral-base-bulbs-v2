4899d25c5f4854b24563e07bfbe8161ea6d8cba2
Eric Rovell <ericrovell@gmail.com>
2020-01-11 00:33:48 +0300

    developing Digits and Number components

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,10 +1,14 @@
 import LayoutMain from "components/Layout/Main/LayoutMain";
 
+import Digits from "components/Digits/Digits";
+import Number from "components/Number/Number";
+
 const HomePage = () => {
   
   return (
     <LayoutMain>
-      <div>Welcome to Next.js!</div>
+      <Digits />
+      <Number />
     </LayoutMain>
   );
 };
