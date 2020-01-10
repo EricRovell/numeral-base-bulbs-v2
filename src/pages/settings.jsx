653d22fc49a76d143bf14bec656dce4f3f7a5b29
Eric Rovell <ericrovell@gmail.com>
2020-01-10 19:35:29 +0300

    testing work of StateProvider

--- src/pages/settings.jsx
+++ src/pages/settings.jsx
@@ -1,8 +1,12 @@
+import { useStateContext } from "components/StateProvider/Context"; 
+
 const SettingsPage = () => {
+
+  const [ { theme }, dispatch ] = useStateContext();
   
   return (
     <main>
-      <div>Settings Page</div>
+      <div>Settings Page, theme is {theme}</div>
     </main>
   );
 };
