985cfb2f35048cf060fce2bb438bc485436c6173
Eric Rovell <ericrovell@gmail.com>
2020-01-11 21:31:02 +0300

    Using EmptyState

--- src/components/Digits/Digits.jsx
+++ src/components/Digits/Digits.jsx
@@ -1,6 +1,7 @@
 import { useStateContext } from "components/StateProvider/Context"; 
 
 import Digit from "./Digit/Digit";
+import EmptyState from "./EmptyState/EmptyState";
 
 import style from "./digits.css";
 
@@ -8,13 +9,19 @@ const Digits = () => {
 
   const [ { digits }, dispatch ] = useStateContext();
 
-  return (
+  const Numerals = ({ digits }) => (
     <div className={style.digits}>
       {digits.map(digit => (
         <Digit value={digit} />
       ))}
     </div>
-  )
+  );
+
+  return (
+    (digits.length)
+      ? <Numerals digits={digits} />
+      : <EmptyState />
+  );
 };
 
 export default Digits;
