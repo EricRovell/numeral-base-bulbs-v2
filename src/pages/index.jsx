ed32713858e16fb3c1a03e27f36ef9f75bbeb09e
Eric Rovell <ericrovell@gmail.com>
2020-01-11 21:30:35 +0300

    using Switcher

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,11 +3,17 @@ import LayoutMain from "components/Layout/Main/LayoutMain";
 import Digits from "components/Digits/Digits";
 import Number from "components/Number/Number";
 
+import Switcher from "components/Digits/Switcher/Switcher";
+
 const HomePage = () => {
   
   return (
     <LayoutMain>
-      <Digits />
+      <section>
+        <Switcher increment={1} title={"Add a digit"} />
+        <Digits />
+        <Switcher increment={-1} title={"Remove a digit"} />
+      </section>      
       <Number />
     </LayoutMain>
   );
