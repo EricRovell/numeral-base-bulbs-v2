d6deeb1db7b803789851a36e454ef6fb9123728d
Eric Rovell <ericrovell@gmail.com>
2020-01-11 21:29:52 +0300

    handling input

--- src/components/Number/Number.jsx
+++ src/components/Number/Number.jsx
@@ -1,13 +1,22 @@
 import style from "./number.css";
 
+import { useState } from "react";
+
 const Number = () => {
+
+  const [ inputValue, setInputValue ] = useState(0);
+
+  const handleInput = event => {
+    setInputValue(event.target.value);
+  }
 
   return (
     <div className={style.number}>
       <div></div>
       <button></button>
       <button></button>
-      <input type="text" value={123456}/>
+      <input type="text" onInput={handleInput} value={inputValue} />
+      {/* <span data-base={12}>{inputValue}</span> */}
     </div>
   );
 };
