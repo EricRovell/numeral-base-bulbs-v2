416c12d49e6662c35a398d32c212c21138f8854f
Eric Rovell <ericrovell@gmail.com>
2020-01-13 00:27:06 +0300

    better Incrementor props handling

--- src/components/Digits/Digit/Digit.jsx
+++ src/components/Digits/Digit/Digit.jsx
@@ -1,4 +1,4 @@
-import { useStateContext } from "components/StateProvider/Context"; 
+//import { useStateContext } from "components/StateProvider/Context"; 
 
 import Label from "./Label";
 import Incrementor from "./Incrementor";
@@ -9,15 +9,15 @@ import style from "./digit.css";
 
 const Digit = ({ value, index }) => {
 
-  //const [ { baseIn }, dispatch ] = useStateContext(); 
+  //const [ { digits }, dispatch ] = useStateContext(); 
 
   return (
     <div className={style.digit}>
       <div></div>
       <Label />
       <Label />
-      <Incrementor actionType={1} index={index} value={value} />
-      <Incrementor actionType={-1} index={index} value={value} />
+      <Incrementor {...{ actionType: 1, index, value }} />
+      <Incrementor {...{ actionType: -1, index, value }} />
       <InputDigit value={value} />
     </div>
   );
