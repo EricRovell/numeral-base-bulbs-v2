9090851f38d45a24e16e189560b5965bf2443cd3
Eric Rovell <ericrovell@gmail.com>
2020-01-13 00:26:47 +0300

    utility abs imports

--- next.config.js
+++ next.config.js
@@ -12,6 +12,7 @@ module.exports = withCSS({
     config.resolve.alias["components"] = path.join(__dirname, "src/components");
     config.resolve.alias["style"] = path.join(__dirname, "src/style");
     config.resolve.alias["pages"] = path.join(__dirname, "src/pages");
+    config.resolve.alias["utility"] = path.join(__dirname, "src/utility");
 
     return config;
   },
