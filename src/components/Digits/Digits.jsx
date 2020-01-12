b118ad4e73a799a9585ab5cad5d7a5f1161d148c
Eric Rovell <ericrovell@gmail.com>
2020-01-12 12:16:22 +0300

    Using Symbolic

--- src/components/Digits/Digits.jsx
+++ src/components/Digits/Digits.jsx
@@ -1,25 +1,15 @@
 import { useStateContext } from "components/StateProvider/Context"; 
 
-import Digit from "./Digit/Digit";
+import Symbolic from "./Symbolic/Symbolic";
 import EmptyState from "./EmptyState/EmptyState";
-
-import style from "./digits.css";
 
 const Digits = () => {
 
   const [ { digits }, dispatch ] = useStateContext();
 
-  const Numerals = ({ digits }) => (
-    <div className={style.digits}>
-      {digits.map(digit => (
-        <Digit value={digit} />
-      ))}
-    </div>
-  );
-
   return (
     (digits.length)
-      ? <Numerals digits={digits} />
+      ? <Symbolic digits={digits} />
       : <EmptyState />
   );
 };
