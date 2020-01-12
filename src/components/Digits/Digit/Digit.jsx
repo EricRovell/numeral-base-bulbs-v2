aaf25b68bb727854afdcfa500a38e182478319ba
Eric Rovell <ericrovell@gmail.com>
2020-01-12 12:17:31 +0300

    using Incrementor

--- src/components/Digits/Digit/Digit.jsx
+++ src/components/Digits/Digit/Digit.jsx
@@ -1,3 +1,5 @@
+import { useStateContext } from "components/StateProvider/Context"; 
+
 import Label from "./Label";
 import Incrementor from "./Incrementor";
 import InputDigit from "./InputDigit";
@@ -5,15 +7,17 @@ import InputDigit from "./InputDigit";
 import style from "./digit.css";
 
 
-const Digit = ({ value }) => {
+const Digit = ({ value, index }) => {
+
+  //const [ { baseIn }, dispatch ] = useStateContext(); 
 
   return (
     <div className={style.digit}>
       <div></div>
       <Label />
       <Label />
-      <Incrementor />
-      <Incrementor />
+      <Incrementor actionType={1} index={index} value={value} />
+      <Incrementor actionType={-1} index={index} value={value} />
       <InputDigit value={value} />
     </div>
   );
