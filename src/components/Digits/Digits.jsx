7685e6c6b488e4002898d9b9df1e8c89af01ff1b
Eric Rovell <ericrovell@gmail.com>
2020-01-14 00:07:35 +0300

    using digits wrapper

--- src/components/Digits/Digits.jsx
+++ src/components/Digits/Digits.jsx
@@ -3,14 +3,18 @@ import { useStateContext } from "components/StateProvider/Context";
 import Symbolic from "./Symbolic/Symbolic";
 import EmptyState from "./EmptyState/EmptyState";
 
+import style from "./digits.css"
+
 const Digits = () => {
 
   const [ { digits }, dispatch ] = useStateContext();
 
   return (
-    (digits.length)
-      ? <Symbolic digits={digits} />
-      : <EmptyState />
+    <div className={style.digits}>
+      {(digits.length)
+        ? <Symbolic digits={digits} />
+        : <EmptyState />}
+    </div>    
   );
 };
 
