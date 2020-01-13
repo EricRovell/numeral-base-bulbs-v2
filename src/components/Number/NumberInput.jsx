41c8f1c350d6abde26b7656630f81b323240c6a8
Eric Rovell <ericrovell@gmail.com>
2020-01-13 23:45:05 +0300

    NumberInput Component

--- src/components/Number/NumberInput.jsx
+++ src/components/Number/NumberInput.jsx
@@ -1,31 +1,56 @@
 import { useStateContext } from "components/StateProvider/Context"; 
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import { baseConvert } from "utility/baseConvert";
 
 const NumberInput = () => {
 
   const [ { digits, baseIn, baseOut }, dispatch ] = useStateContext();
-  const [ inputValue, setInputValue ] = useState(0);
+
+  const [ value, setValue ] = useState(0);
+
+  useEffect(() => {
+
+    const convertedValue = baseConvert({
+      digits: digits.join(""),
+      baseIn,
+      baseOut,
+      representation: "symbol"
+    }).join("");
+
+    console.log(convertedValue);
+    setValue(convertedValue);
+  }, [ digits, baseIn, baseOut ]);
 
   const regex = /^[A-Z\d]+$/;
 
-  const handleInput = ({ target: { value }}) => {
-    if (value === "") setInputValue(0);
+  const handleChange = event => {
+    
+    const inputValue = event.target.value;
+    if (inputValue === "") {
+      dispatch({
+        type: "setDigits",
+        digits: []
+      });
+    };
+    
+    if (regex.test(inputValue)) {
+      setValue(inputValue);
 
-    if (regex.test(value)) {
-      setInputValue(value);
-    }    
+      dispatch({
+        type: "setDigits",
+        digits: baseConvert({
+          digits: inputValue,
+          baseIn: baseOut,
+          baseOut: baseIn,
+          representation: "symbol",
+        })
+      });
+    }  
   };
 
-  const convertedValue = baseConvert({
-    digits,
-    baseIn,
-    baseOut
-  });
-
   return (
-    <input type="text" onChange={handleInput} value={convertedValue} />
+    <input type="text" onChange={handleChange} value={value} />
   );
 };
 
