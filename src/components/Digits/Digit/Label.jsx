8a06ddecc7c23c28cff8066fd168a93f45b1f7f0
Eric Rovell <ericrovell@gmail.com>
2020-01-16 12:34:19 +0300

    showing information based in trueIndex

--- src/components/Digits/Digit/Label.jsx
+++ src/components/Digits/Digit/Label.jsx
@@ -1,5 +1,30 @@
-const Label = () => (
-  <span>0</span>
-);
+import { useStateContext } from "components/StateProvider/Context"; 
+
+const Label = ({ index, type }) => {
+
+  const [ { digits, labelsUp, labelsDown, baseIn }, dispatch ] = useStateContext();
+
+  const trueIndex = digits.length - index - 1;
+  
+  const renderValue = () => {
+    const mode = (type === "up")
+      ? labelsUp
+      : labelsDown
+
+    switch (mode) {
+      case "index":
+        return trueIndex;
+      case "value":
+      default:
+        return baseIn ** trueIndex;
+    }
+  }
+
+  return (
+    <span title={baseIn ** trueIndex}>
+      {renderValue()}
+    </span>
+  );
+};
 
 export default Label;
