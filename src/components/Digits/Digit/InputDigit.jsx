af2f29a74ff99a58210c438fa51b0da946a33405
Eric Rovell <ericrovell@gmail.com>
2020-01-16 12:35:06 +0300

    representing array with shadow values (10 -> A)

--- src/components/Digits/Digit/InputDigit.jsx
+++ src/components/Digits/Digit/InputDigit.jsx
@@ -1,9 +1,19 @@
-const InputDigit = ({ value }) => {
+const InputDigit = ({ value, representation }) => {
+
+  const digit2char = () => {
+    if (representation !== "symbol") {
+      return value;
+    }
+
+    return (value > 9 && value <= 36)
+      ? String.fromCharCode(value + 55)
+      : value
+  };
 
   return (
     <input
       type="text"
-      value={value}
+      value={digit2char()}
       maxLength={1}
       pattern={"[0-9A-Z]"}
     />
