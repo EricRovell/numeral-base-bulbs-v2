f47415593d30cdb43054d286aebd8e95a13de80c
Eric Rovell <ericrovell@gmail.com>
2020-01-17 12:03:30 +0300

    validating digit input

--- src/components/Settings/Base/InputDigit.jsx
+++ src/components/Settings/Base/InputDigit.jsx
@@ -1,11 +1,37 @@
-const InputDigit = ({ value }) => {
+import { useStateContext } from "components/StateProvider/Context"; 
+import { useState } from "react";
+
+import style from "components/Number/number-input.css";
+
+const InputDigit = ({ base, value }) => {
+
+  const [ { baseMin, baseMax }, dispatch ] = useStateContext();
+  const [ isValidInput, setIsValidInput ] = useState(true);
+
+  const handleChange = event => {
+    const userInput = +event.target.value;
+
+    if (Number.isNaN(userInput) || userInput < baseMin || userInput > baseMax) {
+      setIsValidInput(false);
+      setTimeout(() => {
+        setIsValidInput(true);
+      }, 820);
+      return;
+    }
+
+    dispatch({
+      type: "setBase",
+      base,
+      value: userInput
+    });
+  };
 
   return (
     <input
       type="text"
+      className={(isValidInput) ? null : style["number-input"]}
+      onChange={handleChange}
       value={value}
-      maxLength={1}
-      pattern={"[0-9A-Z]"}
     />
   );
 };
