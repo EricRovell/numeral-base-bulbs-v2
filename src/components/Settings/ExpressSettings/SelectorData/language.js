212a787778144c8775f6e76b26c989579d4eed1f
Eric Rovell <ericrovell@gmail.com>
2020-01-19 12:30:09 +0300

    lang data for Selector

---
+++ src/components/Settings/ExpressSettings/SelectorData/language.js
@@ -0,0 +1,21 @@
+export default {
+  title: {
+    "EN": "Language",
+    "RU": "Язык",
+  },
+  state: "lang",
+  options: [
+    {
+      label: {
+        "EN": "English",
+        "RU": "English"
+      },
+    },
+    {
+      label: {
+        "EN": "Русский",
+        "RU": "Русский"
+      },
+    },
+  ]
+};
