e8964cff713c7090b71a70bb77049cdb3ce9d9f2
Eric Rovell <ericrovell@gmail.com>
2020-01-21 12:11:38 +0300

    label's translation

---
+++ src/components/Digits/Digit/Label/translations.js
@@ -0,0 +1,18 @@
+export default {
+  value: {
+      "EN": "Value",
+      "RU": "Значение"
+  },
+  power: {
+    "EN": "Power's value",
+    "RU": "Значение степени"
+  },
+  index: {
+    "EN": "Index",
+    "RU": "Индекс"
+  },
+  none: {
+    "EN": "Click me to set the label.",
+    "RU": "Кликни на меня, чтобы сменить надпись."
+  },
+};
