24321aaedcf1044b18f556685296522da46802e1
Eric Rovell <ericrovell@gmail.com>
2020-01-21 12:08:27 +0300

    language selector data

---
+++ src/components/Settings/Parameters/Language/data.js
@@ -0,0 +1,23 @@
+export default {
+  title: {
+    "EN": "Language",
+    "RU": "Язык",
+  },
+  property: "lang",
+  options: [
+    {
+      label: {
+        "EN": "English",
+        "RU": "English"
+      },
+      value: "EN",
+    },
+    {
+      label: {
+        "EN": "Русский",
+        "RU": "Русский"
+      },
+      value: "RU",
+    },
+  ]
+};
