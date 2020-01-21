a74f5eb87fdcd751771e99d41d8fa5757b9fb795
Eric Rovell <ericrovell@gmail.com>
2020-01-21 12:12:10 +0300

    moved to folder

---
+++ src/components/Digits/Digit/Label/Label.jsx
@@ -0,0 +1,31 @@
+import { useStateContext } from "components/StateProvider/Context";
+import LabelType from "./LabelType";
+import nextMode from "./nextMode";
+
+import style from "./label.css";
+
+const Label = ({ index, type }) => {
+
+  const [ { digits, labelsUp, labelsDown, baseIn }, dispatch ] = useStateContext();
+  const trueIndex = digits.length - index - 1;
+
+  const mode = (type === "labelsUp")
+    ? labelsUp
+    : labelsDown;
+
+  const handleClick = () => {
+    dispatch({
+      type: "setLabel",
+      label: type,
+      value: nextMode(mode)
+    });
+  };
+
+  return (
+    <div className={style.label} onClick={handleClick}>
+      <LabelType {...{ mode, trueIndex }} />
+    </div>
+  );
+};
+
+export default Label;
