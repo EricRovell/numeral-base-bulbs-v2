f51b1a5e46c754a6d96e4d02739f92985e759578
Eric Rovell <ericrovell@gmail.com>
2020-01-21 12:08:35 +0300

    theme selector data

---
+++ src/components/Settings/Parameters/Theme/data.js
@@ -0,0 +1,23 @@
+export default {
+  title: {
+    "EN": "Theme",
+    "RU": "Тема оформления",
+  },
+  property: "theme",
+  options: [
+    {
+      label: {
+        "EN": "Dark",
+        "RU": "Тёмная"
+      },
+      value: "dark"
+    },
+    {
+      label: {
+        "EN": "Light",
+        "RU": "Светлая"
+      },
+      value: "light",
+    },
+  ]
+};
