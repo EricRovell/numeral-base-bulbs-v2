649f7b8feebfa705a97382b6e4731f276a4a9b34
Eric Rovell <ericrovell@gmail.com>
2020-01-25 23:39:21 +0300

    render type moved to function to prevent rerendering

--- src/components/Digits/Digits.jsx
+++ src/components/Digits/Digits.jsx
@@ -1,19 +1,36 @@
 import { useStateContext } from "components/StateProvider/Context"; 
 
 import Symbolic from "./Symbolic/Symbolic";
+import BulbsSimple from "./Bulb/Simple/BulbsSimple";
+import BulbsModern from "./Bulb/Modern/BulbsModern";
 import EmptyState from "./EmptyState/EmptyState";
 
 import style from "./digits.css"
 
-const Digits = () => {
+const RenderDigits = ({ mode, digits, dispatch }) => {
+  if (!digits.length) {
+    return <EmptyState />;
+  }
 
-  const [ { digits }, dispatch ] = useStateContext();
+  switch (mode) {
+    case "bulb-simple":
+      return <BulbsSimple digits={digits} dispatch={dispatch} />;
+    case "bulb-modern":
+      return <BulbsModern digits={digits} dispatch={dispatch} />
+    case "symbol":
+      return <Symbolic digits={digits} />;
+    default:
+      return <EmptyState />;
+  }
+};
+
+const Digits = ({ digits }) => {
+
+  const [ { mode }, dispatch ] = useStateContext();
 
   return (
     <div className={style.digits}>
-      {(digits.length)
-        ? <Symbolic digits={digits} />
-        : <EmptyState />}
+      <RenderDigits {...{ mode, digits, dispatch }} />
     </div>    
   );
 };
