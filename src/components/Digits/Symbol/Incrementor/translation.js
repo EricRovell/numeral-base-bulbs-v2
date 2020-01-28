405fc65e1ec3a6b065067a49a0698d58c7094ad7
Eric Rovell <ericrovell@gmail.com>
2020-01-28 14:19:41 +0300

    translations for incrementor tips

---
+++ src/components/Digits/Symbol/Incrementor/translation.js
@@ -0,0 +1,10 @@
+export const translation = {
+  "1": {
+    "EN": "Increment a digit",
+    "RU": "Увеличить число на один разряд"
+  },
+  "-1": {
+    "EN": "Decrement a digit",
+    "RU": "Уменьшить число на один разряд"
+  }
+};
