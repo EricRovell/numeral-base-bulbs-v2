5dac06d6bb6e8f847dceb8cbb7bea1137d149f2a
Eric Rovell <ericrovell@gmail.com>
2020-01-29 14:15:14 +0300

    available skin/modes data

---
+++ src/components/Settings/Representation/data.js
@@ -0,0 +1,15 @@
+export const data = [
+  {
+    representation: "bulb",
+    skins: [
+      "default",
+      "modern",
+    ],
+  },
+  {
+    representation: "symbol",
+    skins: [
+      "default",
+    ],
+  },
+];
