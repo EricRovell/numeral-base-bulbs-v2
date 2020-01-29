e2b27f36613e79ad40dac21edaf14e5d9ae72b7e
Eric Rovell <ericrovell@gmail.com>
2020-01-29 14:14:35 +0300

    mode selector component

---
+++ src/components/Settings/Representation/Representation.jsx
@@ -0,0 +1,36 @@
+import SelectSkin from "./SelectSkin";
+
+import { data } from "./data";
+
+import style from "./representation.css";
+
+const RepresentationSelect = ({ mode, skin, dispatch }) => {
+
+  const handleMode = event => {
+    dispatch({
+      type: "setMode",
+      mode: event.target.value
+    });
+  };
+
+  const handleSkin = event => {
+    dispatch({
+      type: "setSkin",
+      skin: event.target.value
+    });
+  };
+
+  return (
+    <div className={style.representation}>
+      <h4>Represenation / Skin</h4>
+      <SelectSkin
+        data={data}
+        mode={mode}
+        skin={skin}
+        handleMode={handleMode}
+        handleSkin={handleSkin} />      
+    </div>
+  );
+};
+
+export default RepresentationSelect;
