6408b34a9ec420213f9682bd0b50fae38198507b
Eric Rovell <ericrovell@gmail.com>
2020-01-30 12:22:28 +0300

    using Sandbox Component

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,31 +1,9 @@
-import { useStateContext } from "components/StateProvider/Context";
-
-import LayoutMain from "components/Layout/Main/LayoutMain";
-
-import Digits from "components/Digits/Digits";
-import Number from "components/Number/Number";
-
-import Switcher from "components/Digits/Switcher/Switcher";
-
-import BaseSelector from "components/Settings/Base/BaseSelector";
+import Sandbox from "components/Mode/Sandbox/Sandbox";
 
 const HomePage = () => {
 
-  const [ { digits }, dispatch ] = useStateContext();
-  
   return (
-    <LayoutMain>
-      <section>
-        <Switcher increment={1} />
-        <Digits digits={digits} />
-        <Switcher increment={-1} />
-      </section>      
-      <section>
-        <BaseSelector base={"baseIn"} />
-        <Number />
-        <BaseSelector base={"baseOut"} />
-      </section>
-    </LayoutMain>
+    <Sandbox />
   );
 };
 
