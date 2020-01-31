f1116d1d7a03a54fb57522a9c6114a06c3e20aa6
Eric Rovell <ericrovell@gmail.com>
2020-01-31 03:08:30 +0300

    Using BulbLampSkin

--- src/components/Digits/Bulb/Bulb.jsx
+++ src/components/Digits/Bulb/Bulb.jsx
@@ -1,12 +1,15 @@
 import Label from "../Label/Label";
+
 import BulbDefaultSkin from "./Skins/Default";
 import BulbModernSkin from "./Skins/Modern";
+import BulbLampSkin from "./Skins/Lamp";
 
-import style from "./Simple/bulb-simple.css";
+import style from "./bulb.css";
 
 const bulbSkin = props => ({
   "default": <BulbDefaultSkin {...props} />,
-  "modern": <BulbModernSkin {...props} />
+  "modern": <BulbModernSkin {...props} />,
+  "lamp": <BulbLampSkin {...props} />
 });
 
 const Bulb = ({ skin, value, index, dispatch }) => {
@@ -20,7 +23,7 @@ const Bulb = ({ skin, value, index, dispatch }) => {
   };
 
   return (
-    <div className={style["bulb-simple"]}>
+    <div className={style.bulb}>
       <Label
         index={index}
         type={"labelsUp"} />
