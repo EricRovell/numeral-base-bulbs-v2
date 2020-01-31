c5884bd5cb5baa0c08b68606af90e3828fccd7df
Eric Rovell <ericrovell@gmail.com>
2020-01-31 03:07:49 +0300

    Lamp Bulb Skin

---
+++ src/components/Digits/Bulb/Skins/Lamp.jsx
@@ -0,0 +1,15 @@
+import style from "./lamp.css";
+
+export default ({ value, index, handleSwitch }) => (
+  <div className={style.lamp}>
+    <input
+      type="checkbox" 
+      name="switch"
+      checked={(value === 1)}
+      onClick={handleSwitch}
+      id={`lamp-${index}`} />
+    <div className={style.wire}>
+      <label htmlFor={`lamp-${index}`} />
+    </div>
+  </div>
+);
