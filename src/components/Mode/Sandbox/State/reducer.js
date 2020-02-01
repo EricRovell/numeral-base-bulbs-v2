4824581dae6b3088e4716d382f0ac31441739fbe
Eric Rovell <ericrovell@gmail.com>
2020-02-02 02:10:04 +0300

    Sandbox Context State

---
+++ src/components/Mode/Sandbox/State/reducer.js
@@ -0,0 +1,85 @@
+export default (state, action) => {
+  switch (action.type) {
+
+    // universal (careful)
+
+    case "setStateProperty": 
+      return {
+        ...state,
+        [action.property]: action.value
+      };
+
+    // User Preferences
+
+    case "setLanguage":
+      return {
+        ...state,
+        [action.option]: action.language,
+      };  
+
+    case "setTheme":
+      return {
+        ...state,
+        theme: action.theme,
+      };
+
+
+    // Digits
+      
+    case "addDigit":
+      return {
+        ...state,
+        digits: (action.value === 1)
+          ? [ 0, ...state.digits]
+          : state.digits.slice(1)
+      };
+
+    case "mutateDigit":
+      return {
+        ...state,
+        digits: state.digits.map((val, i) =>
+          (i === action.index) ? action.value : val)
+      };
+
+    case "setDigits":
+      return {
+        ...state,
+        digits: action.digits
+      };
+
+    // base
+
+    case "setBase":
+      return {
+        ...state,
+        [action.base]: action.value
+      };
+
+    // labels
+
+    case "setLabel":
+      return {
+        ...state,
+        [action.label]: action.value
+      };
+
+    // mode / skin
+    case "setMode": {
+      return {
+        ...state,
+        skin: "default",
+        mode: action.mode || "symbol"
+      }
+    }
+
+    case "setSkin": {
+      return {
+        ...state,
+        skin: action.skin || "default",
+      }
+    }
+
+    default:
+      return state;
+  }
+};
---
+++ src/components/Mode/Sandbox/State/useStateSandbox.js
@@ -0,0 +1,14 @@
+import { createContext, useContext, useReducer } from "react";
+
+export const Context = createContext(null);
+
+export const StateProvider = ({ reducer, defaultState, children }) => (
+  <Context.Provider value={useReducer(reducer, defaultState)}>
+    {children}
+  </Context.Provider>
+);
+
+export const useStateSandbox = () => useContext(Context);
+
+export { default as defaultState } from "./defaultState.js";
+export { default as reducer } from "./reducer.js";
