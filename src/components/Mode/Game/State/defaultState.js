ae44da12e6667b8aec9185c16787439ecb492a26
Eric Rovell <ericrovell@gmail.com>
2020-02-02 11:39:53 +0300

    GameMode State Context

---
+++ src/components/Mode/Game/State/defaultState.js
@@ -0,0 +1,17 @@
+export default {
+  digits: [0, 0, 0, 0, 0, 0, 0, 0],
+  mode: "symbol", // symbol
+  skin: "default",
+
+  digitsMin: 0,
+  digitsMax: 16,
+  digitsDefault: 8,
+  
+  baseIn: 2,
+  baseOut: 10,
+  baseMin: 2,
+  baseMax: 16,
+
+  labelsUp: "value",    // index | value | power | none
+  labelsDown: "index",
+};
