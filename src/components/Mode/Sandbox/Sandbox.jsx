1f2c17288236b3cd553e96492aa4b4de26b80587
Eric Rovell <ericrovell@gmail.com>
2020-02-02 11:41:31 +0300

    using ExpressSettings

--- src/components/Mode/Sandbox/Sandbox.jsx
+++ src/components/Mode/Sandbox/Sandbox.jsx
@@ -4,13 +4,16 @@ import LayoutMain from "components/Layout/Main/LayoutMain";
 import DigitsSection from "./Sections/DigitsSection";
 import NumberSection from "./Sections/NumberSection";
 
+import ExpressSettings from "components/Settings/ExpressSettings/ExpressSettings";
+
 const Sandbox = () => {
 
   return (
     <StateProvider {...{ defaultState, reducer }}>
       <LayoutMain>        
         <DigitsSection />
-        <NumberSection />        
+        <NumberSection />
+        <ExpressSettings />        
       </LayoutMain>
     </StateProvider>
   );
