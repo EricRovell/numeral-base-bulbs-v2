c7997ede0b63b35437dcd7be0c38a564a17e613f
Eric Rovell <ericrovell@gmail.com>
2020-02-04 12:20:54 +0300

    refactored to be more concise

--- src/components/Digits/Digits.jsx
+++ src/components/Digits/Digits.jsx
@@ -6,26 +6,22 @@ import EmptyState from "./EmptyState/EmptyState";
 
 import style from "./digits.css"
 
-const RenderDigits = ({ mode, skin, digits, dispatch, baseIn }) => {
-  if (!digits.length) return <EmptyState />;
-
-  switch (mode) {
-    case "bulb":
-      return <Bulbs {...{ digits, skin, dispatch }} />
-    case "symbol":
-      return <Symbols {...{ digits, skin, baseIn, dispatch }} />;
-    default:
-      return <EmptyState />;
-  }
-};
+const renderDigits = (props) => ({
+  "bulb": <Bulbs {...props} />,
+  "symbol": <Symbols {...props} />,
+});
 
 const Digits = ({ digits }) => {
 
   const [ { mode, skin, baseIn }, dispatch ] = useStateSandbox();
 
+  if (!digits.length) {
+    return <EmptyState dispatch={dispatch} />;
+  }
+
   return (
     <div className={style.wrapper}>
-      <RenderDigits {...{ mode, digits, baseIn, dispatch, skin }} />
+      {renderDigits({ digits, skin, baseIn, dispatch })[mode]}
     </div>    
   );
 };
