7ab51193dbf9d0a7d5aebfaf6eb5ece0793b340c
Eric Rovell <ericrovell@gmail.com>
2020-02-04 12:25:02 +0300

    removed non-global values

--- src/components/StateProvider/initialState.js
+++ src/components/StateProvider/initialState.js
@@ -1,22 +1,4 @@
 export default {
   lang: "EN",
   theme: "dark",
-
-  digits: [0, 0, 0, 0, 0, 0, 0, 0],
-  //representation: "symbol",  
-  mode: "symbol", // symbol
-  skin: "default",
-
-  digitsMin: 0,
-  digitsMax: 16,
-  digitsDefault: 8,
-  
-  baseIn: 2,
-  baseOut: 10,
-  baseMin: 1,
-  baseMax: 16,
-
-  labelsMutate: true,
-  labelsUp: "value",    // index | value | power | none
-  labelsDown: "index",
 };
