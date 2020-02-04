0c38db6f721a5593905242777b05f020eb1306b6
Eric Rovell <ericrovell@gmail.com>
2020-02-04 12:24:07 +0300

    settings, drafts, wip

---
+++ src/components/Settings/Settings/data.js
@@ -0,0 +1,19 @@
+export default [
+  {
+    title: "Preferences",
+    // language, theme, header lamp
+  },
+  {
+    title: "Digits",
+    // max min how many by default
+    // repr/skin by default
+  },
+  {
+    title: "Numeral base",
+    // user boundaries and default values
+  },
+  {
+    title: "Labels",
+    // default values nad should they be mutated
+  },
+];
--- src/pages/settings.jsx
+++ src/pages/settings.jsx
@@ -1,12 +1,39 @@
-import { useStateContext } from "components/StateProvider/Context"; 
+//import { useStateContext } from "components/StateProvider/Context";
+
+import style from "style/pages/settings.css";
 
 const SettingsPage = () => {
 
-  const [ { theme }, dispatch ] = useStateContext();
+  //const [ { theme }, dispatch ] = useStateContext();
   
   return (
-    <main>
-      <div>Settings Page, theme is {theme}</div>
+    <main className={style.settings}>
+      <aside>
+        <a href="#user-settings-1">settings 1</a>
+        <a href="#user-settings-2">settings 2</a>
+        <a href="#user-settings-3">settings 3</a>
+        <a href="#user-settings-4">settings 4</a>
+      </aside>
+      <div>
+        <section id="user-settings-1">
+          <h2>User Settings</h2>
+        </section>
+        <section id="user-settings-2">
+          <h2>User Settings</h2>
+        </section>
+        <section id="user-settings-3">
+          <h2>User Settings</h2>
+        </section>
+        <section id="user-settings-4">
+          <h2>User Settings</h2>
+        </section>
+        <section id="user-settings-5">
+          <h2>User Settings</h2>
+        </section> 
+        <section id="user-settings-6">
+          <h2>User Settings</h2>
+        </section>  
+      </div>     
     </main>
   );
 };
