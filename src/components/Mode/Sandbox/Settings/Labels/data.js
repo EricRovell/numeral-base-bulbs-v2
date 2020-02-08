ec61f491342fb0bcd0039696fbde1657be3d7c1d
Eric Rovell <ericrovell@gmail.com>
2020-02-09 01:17:25 +0300

    labels settings controls data

---
+++ src/components/Mode/Sandbox/Settings/Labels/data.js
@@ -0,0 +1,14 @@
+export const labelsUp = {
+  label: "Labels-Up",
+  options: [
+    "index",
+    "value",
+    "power",
+    "none",
+  ],
+};
+
+export const labelsDown = {
+  ...labelsUp,
+  label: "Labels-Down",
+};
