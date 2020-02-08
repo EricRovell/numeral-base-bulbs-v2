b446e9fdb77b55691e2726f8cdd9f4888284d07e
Eric Rovell <ericrovell@gmail.com>
2020-02-09 01:17:15 +0300

    digits settings controls data

---
+++ src/components/Mode/Sandbox/Settings/Digits/data.js
@@ -0,0 +1,17 @@
+export const digits = {
+  label: "Digits",
+  minLength: 0,
+  maxLength: 16,
+};
+
+export const minDigits = {
+  label: "minimum Digits",
+  min: 0,
+  max: 36,
+};
+
+export const maxDigits = {
+  label: "maximum Digits",
+  min: 0,
+  max: 36,
+};
