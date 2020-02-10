4423441e59bfaaf3ed0b99dde4b29bf41e15128d
Eric Rovell <ericrovell@gmail.com>
2020-02-10 14:04:05 +0300

    set another names to prevent collision

--- src/components/Mode/Sandbox/Settings/Digits/data.js
+++ src/components/Mode/Sandbox/Settings/Digits/data.js
@@ -1,16 +1,16 @@
-export const digits = {
+export const defDigits = {
   label: "Digits",
   minLength: 0,
   maxLength: 16,
 };
 
-export const minDigits = {
+export const defMinDigits = {
   label: "minimum Digits",
   min: 0,
   max: 36,
 };
 
-export const maxDigits = {
+export const defMaxDigits = {
   label: "maximum Digits",
   min: 0,
   max: 36,
