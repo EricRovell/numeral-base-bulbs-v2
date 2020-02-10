6ec2dfb1950caa449a1586dc1ea3f7fd86374b39
Eric Rovell <ericrovell@gmail.com>
2020-02-10 14:02:59 +0300

    controlled state and name prop

--- src/components/Settings/Controls/Select.jsx
+++ src/components/Settings/Controls/Select.jsx
@@ -1,17 +1,18 @@
-const Select = ({ data, value }) => {
-
-  return (
-    < >
-      <label>{data.label}</label>        
-        <select defaultValue={value}>
+const Select = ({ data, value, name, handleInputChange }) => (
+  < >
+    <label>{data.label}</label>        
+      <select
+        defaultValue={value}
+        value={value}
+        onChange={handleInputChange}
+        name={name}>
           {data.options.map((option, i) => (
             <option value={option} key={option}>
               {option}
             </option>
           ))}
-        </select>      
-    </>
-  );
-};
+      </select>      
+  </>
+);
 
 export default Select;
