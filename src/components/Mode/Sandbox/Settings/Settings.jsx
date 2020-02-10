6ab80fc87142e3087a095f7d9c8af88900f5798b
Eric Rovell <ericrovell@gmail.com>
2020-02-10 14:04:44 +0300

    handleInputChange; using reducer to keep state

--- src/components/Mode/Sandbox/Settings/Settings.jsx
+++ src/components/Mode/Sandbox/Settings/Settings.jsx
@@ -5,39 +5,44 @@ import Base from "./Base/Base";
 import Digits from "./Digits/Digits";
 import Representation from "./Representation/Representation";
 
-import Button from "components/Settings/Controls/Button";
-
 import defaultState from "../State/defaultState";
+import validationReducer from "./reducerValidate";
 
 import style from "./settings.css";
 
-const data = {
-  title: "Reset to defaults",
-};
-
-const reducer = (state, action) => {
-  return {
-    ...state,
-    ...action
-  };
-};
 
 const SettingsSandbox = () => {
 
-  const [ state, dispatch ] = useReducer(reducer, defaultState);
+  const [ state, dispatch ] = useReducer(validationReducer, defaultState);
+
+  const handleInputChange = ({ target }) => {
+    console.log(target.name, target.value);
+
+    dispatch({
+      name: target.name,
+      value: target.value 
+    });
+  };
 
   return (    
     <form className={style.settings}>
-      <Button data={data} />
+      <Digits
+        digits={state.digits}
+        digitsMin={state.digitsMin}
+        digitsMax={state.digitsMax}
+        handleInputChange={handleInputChange} />        
+      <Base
+        baseIn={state.baseIn}
+        baseOut={state.baseOut}
+        handleInputChange={handleInputChange} />
+      <Representation
+        mode={state.mode}
+        skin={state.skin}
+        handleInputChange={handleInputChange} />
       <Labels
         valueUp={state.labelsUp}
         valueDown={state.labelsDown}
-        dispatch={dispatch} />
-      <Base
-        baseIn={state.baseIn}
-        baseOut={state.baseOut} />
-      <Digits />
-      <Representation />
+        handleInputChange={handleInputChange} />    
     </form>    
   );
 };
