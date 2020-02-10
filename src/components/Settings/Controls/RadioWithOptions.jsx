f3c3caa204a73efe65fe11007358f31ae518278f
Eric Rovell <ericrovell@gmail.com>
2020-02-10 14:02:46 +0300

    controlled state and name prop

--- src/components/Settings/Controls/RadioWithOptions.jsx
+++ src/components/Settings/Controls/RadioWithOptions.jsx
@@ -1,20 +1,27 @@
-const RadioWithOptions = ({ data, name }) => {
-
-  return (
-    < >
-      <input type="radio" name={name} />
-      <label>
-        {data.representation}
-      </label>
-      <select>
+const RadioWithOptions = ({ data, nameRadio, nameSelect, valueRadio, valueSelect, handleInputChange }) => (
+  < >
+    <input
+      type="radio"
+      name={nameRadio}
+      value={data.representation}
+      defaultChecked={(data.representation === valueRadio)}
+      onChange={handleInputChange} />
+    <label>
+      {data.representation}
+    </label>
+    <select
+      name={nameSelect}
+      onChange={handleInputChange}
+      defaultValue={valueSelect}
+      value={valueSelect}
+      disabled={(data.representation !== valueRadio)}>
         {data.skins.map(option => (
           <option value={option} key={option}>
             {option}
           </option>
         ))}
-      </select>   
-    </>
-  );
-};
+    </select>   
+  </>
+);
 
 export default RadioWithOptions;
