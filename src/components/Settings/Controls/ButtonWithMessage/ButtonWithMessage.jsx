a6371e3ff6fd611edab12b634881ee6e64d501fa
Eric Rovell <ericrovell@gmail.com>
2020-02-12 14:43:11 +0300

    atomic button component with active message state

---
+++ src/components/Settings/Controls/ButtonWithMessage/ButtonWithMessage.jsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import style from "./button-with-message.css";
+
+const Button = ({ title, action, handleClick }) => {
+
+  const [ isActive, setIsActive ] = useState(false);
+
+  const setAction = event => {
+    event.preventDefault();
+    setIsActive(true);
+    handleClick();
+
+    setTimeout(() => {
+      setIsActive(false);
+    }, 1250);
+  }
+  
+  return (
+    <button
+      onClick={setAction}
+      className={`
+        ${style["button-with-icon"]}
+        ${isActive && style["button-with-icon--active"]}`}>
+          <div>
+            <span>{title}</span>
+            <span>
+              {action}
+            </span>
+          </div>
+    </button>
+  );
+};
+
+export default Button;
