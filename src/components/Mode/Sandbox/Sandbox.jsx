32ae7c4197addec541b5268c4a8ad98a015207b2
Eric Rovell <ericrovell@gmail.com>
2020-02-12 14:47:31 +0300

    using useUserSettings and Loader

--- src/components/Mode/Sandbox/Sandbox.jsx
+++ src/components/Mode/Sandbox/Sandbox.jsx
@@ -1,4 +1,7 @@
+import useUserSettings from "components/Settings/useUserSettings/useUserSettings";
 import { StateProvider, defaultState, reducer } from "./State/useStateSandbox";
+
+import Loader from "components/Loader/Loader";
 
 import LayoutMain from "components/Layout/Main/LayoutMain";
 import DigitsSection from "./Sections/DigitsSection";
@@ -6,17 +9,25 @@ import NumberSection from "./Sections/NumberSection";
 
 import ExpressSettings from "components/Settings/ExpressSettings/ExpressSettings";
 
+
 const Sandbox = () => {
 
-  return (
-    <StateProvider {...{ defaultState, reducer }}>
+  const [ isLoading, userSettings ] = useUserSettings("SettingsSandbox");
+
+  if (isLoading) {
+    return <Loader />
+  }
+
+  return !isLoading &&
+    <StateProvider
+    defaultState={{...defaultState, ...userSettings}}
+    reducer={reducer}>
       <LayoutMain>        
         <DigitsSection />
         <NumberSection />
         <ExpressSettings />        
       </LayoutMain>
-    </StateProvider>
-  );
+    </StateProvider>  
 };
 
 export default Sandbox;
