9340a7f097c136dbad8953e80c8f4880cb4a8f9f
Eric Rovell <ericrovell@gmail.com>
2020-02-12 14:43:39 +0300

    Loader Component

---
+++ src/components/Loader/Loader.jsx
@@ -0,0 +1,11 @@
+import style from "./loader.css";
+
+const Loader = () => (
+  <div className={style.loader}>    
+    <span />
+    <span />
+    <span />          
+  </div>
+);
+
+export default Loader;
