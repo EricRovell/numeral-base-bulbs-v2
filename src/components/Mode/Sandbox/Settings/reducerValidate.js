3af72d10dfeec1bdef062840a0f6df1c62f8d396
Eric Rovell <ericrovell@gmail.com>
2020-02-12 14:44:59 +0300

    using validation functions

---
+++ src/components/Mode/Sandbox/Settings/reducerValidate.js
@@ -0,0 +1,46 @@
+import { validate } from "./validate";
+
+export default (state, action) => {
+  switch (action.name) {
+
+    // labels
+    case "labelsUp":
+    case "labelsDown":
+      return {
+        ...state,
+        [action.name]: action.value
+      };
+
+    // representation
+    case "mode": {
+      return {
+        ...state,
+        skin: "default",
+        mode: action.value || "symbol"
+      };
+    };
+
+    case "skin": {
+      return {
+        ...state,
+        skin: action.value || "default",
+      };
+    };
+
+    // base
+    case "baseIn":
+    case "baseOut":
+    case "digitsMin":
+    case "digitsMax":
+    case "digits":
+      return {
+        ...state,
+        [action.name]: validate(action.name, action.value, state)
+      };
+
+    
+
+    default:
+      return state;
+  }
+};
