e5a513393b2cb13c343d301494e16a2e028db79e
Eric Rovell <ericrovell@gmail.com>
2020-02-14 03:16:32 +0300

    lang and theme data for controls

---
+++ src/components/StateProvider/SettingsGlobal/data.js
@@ -0,0 +1,35 @@
+export const lang = {
+  label: {
+    "EN": "Language",
+    "RU": "Язык"
+  },
+  name: "lang",
+  options: [
+    {
+      title: "English",
+      value: "EN"
+    },
+    {
+      title: "Русский",
+      value: "RU"
+    }
+  ],
+};
+
+export const theme = {
+  label: {
+    "EN": "Theme",
+    "RU": "Тема оформления"
+  },
+  name: "theme",
+  options: [
+    {
+      title: "Dark",
+      value: "dark"
+    },
+    {
+      title: "Light",
+      value: "light"
+    }
+  ],
+};
