8c02338efdb59cb167c08b3249830fa872ab5533
Eric Rovell <ericrovell@gmail.com>
2020-02-14 03:12:53 +0300

    Moved Section to its folder

--- src/components/Settings/Section.jsx
+++ src/components/Settings/Section.jsx
@@ -1,5 +1,7 @@
+import style from "./section.css";
+
 const SettingsSection = ({ children, name }) => (
-  <div>
+  <div className={style.section}>
     <header>{name}</header>
     {children}
   </div>
