4b241029b06e63ae67f2d8f93434c9c507abe5ba
Eric Rovell <ericrovell@gmail.com>
2020-02-14 03:17:18 +0300

    using nav and layout

--- src/pages/settings.jsx
+++ src/pages/settings.jsx
@@ -1,16 +1,57 @@
-//import { useStateContext } from "components/StateProvider/Context";
+import { useRouter } from "next/router";
+
+import Tabs from "components/UI/Tabs/Tabs";
+import LayoutSettings from "components/Layout/Settings/LayoutSettings";
+
+import SettingsGlobal from "components/StateProvider/SettingsGlobal/SettingsGlobal";
 import SettingsSandbox from "components/Mode/Sandbox/Settings/Settings";
 
 import style from "style/pages/settings.css";
+import styleTabs from "components/Tabs/tabs-settings.css";
+
+const routes = [
+  {
+    path: "/settings?tab=globals",
+    name: {
+      "EN": "Globals",
+      "RU": "Основные",
+    },
+  },
+  {
+    path: "/settings?tab=sandbox",
+    name: {
+      "EN": "Sandbox",
+      "RU": "Песочница",
+    },
+  },
+  {
+    path: "/settings?tab=Game",
+    name: {
+      "EN": "Game",
+      "RU": "Игра",
+    },
+  },
+];
+
+const settingsTab = () => ({
+  "sandbox": <SettingsSandbox />,
+  "globals": <SettingsGlobal />
+});
 
 const SettingsPage = () => {
 
-  //const [ { theme }, dispatch ] = useStateContext();
+  const { query: { tab, lang }} = useRouter();
   
   return (
-    <main className={style.settings}>
-       <SettingsSandbox />   
-    </main>
+    <LayoutSettings className={style.settings}>
+      <div>
+        <Tabs
+          routes={routes}
+          style={styleTabs}
+          lang={lang || "EN"} />
+      </div>
+      {settingsTab()[tab]}
+    </LayoutSettings>
   );
 };
 
