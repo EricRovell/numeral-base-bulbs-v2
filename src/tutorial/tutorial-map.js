aa26895782193f038619bef5914c87e2ac380d55
Eric Rovell <ericrovell@gmail.com>
2020-02-20 02:41:37 +0300

    tutorial map, wip

---
+++ src/tutorial/tutorial-map.js
@@ -0,0 +1,32 @@
+export default {
+  "/": {
+    title: "Welcome to Numeral Base Bulbs!",
+    mdx: "./welcome.mdx",
+  },
+  // bases section
+  "/bases": {
+    title: "Let's talk about bases and their types",
+    mdx: "./bases/numeral-bases.mdx",
+  },
+  "/bases/non-positional": {
+    title: "Non-positional numeral bases",
+    path: "./bases/non-positional.mdx",
+  },
+  "/bases/positional": {
+    title: "Positional numeral bases",
+    mdx: "./bases/positional.mdx",
+  },
+  "/bases/positional/homogeneous": {
+    title: "Homogeneous positional numeral systems",
+    mdx: "./bases/positional/"
+  },
+  "/bases/positional/mixed": "Mixed positional numeral systems",
+  "/bases/positional/special": "Special positional numeral systems",
+  // transformations section
+  "/transformation": "How to transform numbers",
+  "/transformtation": "10 -> 2",
+  // How to use this App
+  "/app/sandbox": "Sandbox Tutorial",
+  "/app/game": "Game Mode Tutorial",
+  "/app/settings": "User Preferences Tutorial",
+};
