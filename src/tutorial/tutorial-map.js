cfefd4796df69626f2b0e6875fcee9ad71186be2
Eric Rovell <ericrovell@gmail.com>
2020-02-24 14:37:54 +0300

    updated with new articles

--- src/tutorial/tutorial-map.js
+++ src/tutorial/tutorial-map.js
@@ -3,13 +3,18 @@ import TutorialLanding from "./welcome.mdx";
 // Bases
 import NumeralBases from "./numeral-bases.mdx";
 import NonPositionalBases from "./non-positional.mdx";
+import Positional from "./positional.mdx";
+// Sandbox
+import Sandbox from "./sandbox.mdx";
 
 export default () => ({
+
   // tutorial's landing page
   "ru": {
     title: "Welcome to the Numeral Base Bulbs!",
     mdx: <TutorialLanding />,
   },
+
   // bases section
   "ru/bases": {
     title: "Let's talk about bases and their types",
@@ -19,21 +24,16 @@ export default () => ({
     title: "Non-positional numeral bases",
     mdx: <NonPositionalBases />,
   },
-  /* "/bases/positional": {
+  "ru/bases/positional": {
     title: "Positional numeral bases",
-    mdx: "./bases/positional.mdx",
+    mdx: <Positional />,
   },
-  "/bases/positional/homogeneous": {
-    title: "Homogeneous positional numeral systems",
-    mdx: "./bases/positional/"
+
+  // sandbox
+  "ru/sandbox": {
+    title: "Sandbox Mode Tutorial",
+    mdx: <Sandbox />,
   },
-  "/bases/positional/mixed": "Mixed positional numeral systems",
-  "/bases/positional/special": "Special positional numeral systems",
-  // transformations section
-  "/transformation": "How to transform numbers",
-  "/transformtation": "10 -> 2",
-  // How to use this App
-  "/app/sandbox": "Sandbox Tutorial",
-  "/app/game": "Game Mode Tutorial",
-  "/app/settings": "User Preferences Tutorial", */
+
+
 });
