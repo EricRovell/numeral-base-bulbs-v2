f794cee05c0db9d89abf5b4aa0c7ab07a44beee3
Eric Rovell <ericrovell@gmail.com>
2020-02-26 13:00:25 +0300

    using props instead of context

--- src/components/Digits/Symbol/Incrementor/Incrementor.jsx
+++ src/components/Digits/Symbol/Incrementor/Incrementor.jsx
@@ -1,14 +1,12 @@
 import { useStateContext } from "components/StateProvider/Context";
-import { useStateSandbox } from "components/Mode/Sandbox/State/useStateSandbox";
 
 import IncrementIcon from "./IncrementIcon";
 import IncrementSymbolValue from "./handleIncrement";
 import { translation } from "./translation";
 
-const Incrementor = ({ action, type = "default", index, value }) => {
+const Incrementor = ({ action, type = "default", index, value, baseIn, dispatch }) => {
 
   const [ { lang } ] = useStateContext();
-  const [ { baseIn }, dispatch ] = useStateSandbox();
   
   const handleIncrement = () => {
     IncrementSymbolValue({
