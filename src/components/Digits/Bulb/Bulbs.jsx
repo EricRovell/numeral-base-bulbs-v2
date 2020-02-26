109032066c797b7fbc1665041acc2757ecb05313
Eric Rovell <ericrovell@gmail.com>
2020-02-26 12:58:57 +0300

    providing props all subcomponents need

--- src/components/Digits/Bulb/Bulbs.jsx
+++ src/components/Digits/Bulb/Bulbs.jsx
@@ -1,13 +1,15 @@
 import Bulb from "./Bulb";
 import style from "../digits.module.css";
 
-const Bulbs = ({ digits, skin, dispatch }) => (
+const Bulbs = ({ digits, skin, baseIn, representation, dispatch }) => (
   <div className={style.container}>
     {digits.map((value, index) => (
       <Bulb
         skin={skin}
         value={value}
         index={index}
+        baseIn={baseIn}
+        representation={representation}
         dispatch={dispatch}
         key={`bulb-${index}`} />
     ))}
