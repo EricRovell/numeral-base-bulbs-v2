9f84a8a388c0d748fb9872e5fc1345ccde57e99b
Eric Rovell <ericrovell@gmail.com>
2020-02-26 13:03:04 +0300

    moved in logic with settings, loader, context provider

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,9 +1,23 @@
+import useUserSettings from "components/Settings/useUserSettings/useUserSettings";
+import { StateProvider, defaultState, reducer } from "components/Mode/Sandbox/State/useStateSandbox";
+
 import Sandbox from "components/Mode/Sandbox/Sandbox";
+import Loader from "components/Loader/Loader";
 
 const HomePage = () => {
 
-  return (
-    <Sandbox />
+  const [ state, isLoading ] = useUserSettings(defaultState, "SettingsSandbox");
+
+  if (isLoading) {
+    return <Loader />
+  }
+
+  return !isLoading && (
+    <StateProvider
+      defaultState={state}
+      reducer={reducer}>
+        <Sandbox />
+    </StateProvider>
   );
 };
 
