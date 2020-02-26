64ecec9c56ff0fde905a5c51af123ea2a1035909
Eric Rovell <ericrovell@gmail.com>
2020-02-26 13:02:40 +0300

    moved out settings outside of component

--- src/components/Mode/Sandbox/Sandbox.jsx
+++ src/components/Mode/Sandbox/Sandbox.jsx
@@ -1,33 +1,25 @@
-import useUserSettings from "components/Settings/useUserSettings/useUserSettings";
-import { StateProvider, defaultState, reducer } from "./State/useStateSandbox";
-
-import Loader from "components/Loader/Loader";
-
 import LayoutMain from "components/Layout/Main/LayoutMain";
 import DigitsSection from "./Sections/DigitsSection";
 import NumberSection from "./Sections/NumberSection";
 
 import ExpressSettings from "components/Settings/ExpressSettings/ExpressSettings";
 
+import { useStateSandbox } from "components/Mode/Sandbox/State/useStateSandbox"; 
 
 const Sandbox = () => {
 
-  const [ state, isLoading ] = useUserSettings(defaultState, "SettingsSandbox");
+  const [ state , dispatch ] = useStateSandbox();
 
-  if (isLoading) {
-    return <Loader />
-  }
-
-  return !isLoading && (
-    <StateProvider
-      defaultState={state}
-      reducer={reducer}>
-        <LayoutMain>        
-          <DigitsSection />
-          <NumberSection />
-          <ExpressSettings />        
-        </LayoutMain>
-    </StateProvider>
+  return (
+    <LayoutMain>        
+      <DigitsSection
+        state={state}
+        dispatch={dispatch} />
+      <NumberSection
+        state={state}
+        dispatch={dispatch} />
+      <ExpressSettings />        
+    </LayoutMain>
   );
 };
 
