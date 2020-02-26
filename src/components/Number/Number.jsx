728a41ea6820b15b28af5a37189e3df6d78521e0
Eric Rovell <ericrovell@gmail.com>
2020-02-26 13:03:40 +0300

    using props instead of context

--- src/components/Number/Number.jsx
+++ src/components/Number/Number.jsx
@@ -1,15 +1,19 @@
+import NumberInput from "./NumberInput";
 import style from "./number.module.css";
 
-import NumberInput from "./NumberInput";
-
-const Number = () => {
+const Number = ({ digits, baseIn, baseOut, dispatch }) => {
 
   return (
     <div className={style.number}>
-      <div></div>
-      <button></button>
-      <button></button>
-      <NumberInput />
+      <div />
+      <button />
+      <button />
+      <NumberInput {...{
+        digits,
+        baseIn,
+        baseOut,
+        dispatch
+      }} />
     </div>
   );
 };
