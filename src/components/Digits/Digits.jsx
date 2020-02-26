0b7d2f07df8d5ff7c81d64b0b20d8e08a2ef4d75
Eric Rovell <ericrovell@gmail.com>
2020-02-26 12:58:12 +0300

    using props instead of context

--- src/components/Digits/Digits.jsx
+++ src/components/Digits/Digits.jsx
@@ -1,5 +1,3 @@
-import { useStateSandbox } from "components/Mode/Sandbox/State/useStateSandbox"; 
-
 import Bulbs from "./Bulb/Bulbs";
 import Symbols from "./Symbol/Symbols";
 import EmptyState from "./EmptyState/EmptyState";
@@ -11,17 +9,15 @@ const renderDigits = (props) => ({
   "symbol": <Symbols {...props} />,
 });
 
-const Digits = ({ digits }) => {
+const Digits = (props) => {
 
-  const [ { mode, skin, baseIn }, dispatch ] = useStateSandbox();
-
-  if (!digits.length) {
-    return <EmptyState dispatch={dispatch} />;
+  if (!props.digits.length) {
+    return <EmptyState dispatch={props.dispatch} />;
   }
 
   return (
     <div className={style.wrapper}>
-      {renderDigits({ digits, skin, baseIn, dispatch })[mode]}
+      {renderDigits({ ...props })[props.mode]}
     </div>    
   );
 };
