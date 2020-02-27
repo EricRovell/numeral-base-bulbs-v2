6749ac2c27739b64456cd5e808d6cb89f9a0a50b
Eric Rovell <ericrovell@gmail.com>
2020-02-27 22:03:48 +0300

    set Link Active state for root path (header)

--- src/components/UI/Tabs/Link.js
+++ src/components/UI/Tabs/Link.js
@@ -4,11 +4,34 @@ import { Children } from "react";
 import Link from "next/link";
 import { withRouter } from "next/router";
 
-
 export default withRouter(({ router, children, as, href, className, activeClassName, ...rest }) => (
-   <Link {...rest} href={href} as={as}>
-      {React.cloneElement(Children.only(children), {
-         className: (router.asPath === href || router.asPath === as) ? activeClassName : className
-      })}
-   </Link>
+  <Link {...rest} href={href} as={as}>
+     {React.cloneElement(Children.only(children), {
+        className: (`/${router.pathname.split("/")[1]}` === href) ? activeClassName : className
+     })}
+  </Link>
 ));
+
+/* 
+export default withRouter(({ router, children, as, href, className, activeClassName, ...rest }) => (
+  <Link {...rest} href={href} as={as}>
+     {React.cloneElement(Children.only(children), {
+        className: (router.asPath === href || router.asPath === as) ? activeClassName : className
+     })}
+  </Link>
+)); */
+
+/* export default withRouter(({ linkType = "header", router, children, href, className, activeClassName, ...rest }) => {
+  
+  const setClassName = (linkType === "header")
+    ? (`/${router.pathname.split("/")[1]}` === href) ? activeClassName : className
+    : (router.asPath === href) ? activeClassName : className
+
+  return (
+    <Link {...rest} href={href}>
+      {React.cloneElement(Children.only(children), {
+          className: setClassName
+      })}
+    </Link>
+  );
+}); */
