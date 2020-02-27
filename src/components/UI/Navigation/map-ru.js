ef2539539b82ad108f227c4ddc12c27170163911
Eric Rovell <ericrovell@gmail.com>
2020-02-27 22:04:39 +0300

    tutorial contents data, ru

---
+++ src/components/UI/Navigation/map-ru.js
@@ -0,0 +1,49 @@
+export default [
+  {
+    label: "Содержание",
+    nodes: [
+
+      {
+        label: "Системы счисления",
+        nodes: [
+          {
+            label: "Что же такое система счисления?",
+            href: "/tutorial/ru/bases",
+          },
+          {
+            label: "Непозитионные системы счисления",
+            href: "/tutorial/ru/bases/non-positional",
+          },
+          {
+            label: "Позиционные системы счисления",
+            href: "/tutorial/ru/bases/positional",
+          },
+        ]
+      },
+
+      {
+        label: "Режим песочницы",
+        nodes: [
+          
+          {
+            label: "Режим песочницы, что же это?",
+            href: "/tutorial/ru/sandbox",
+          },
+
+          {
+            label: "Символы",
+            href: "/tutorial/ru/sandbox/symbols",
+          },
+
+          {
+            label: "Число",
+            href: "/tutorial/ru/sandbox/number",
+          },
+
+        ]
+      },
+
+    ],
+  },
+];
+
