54802562589b960602c99f7cd6c0d4a63dfc7fa1
Eric Rovell <ericrovell@gmail.com>
2020-02-27 22:05:54 +0300

    using Navigation

--- src/components/Layout/Article/LayoutArticle.jsx
+++ src/components/Layout/Article/LayoutArticle.jsx
@@ -1,12 +1,28 @@
+import Navigation from "components/UI/Navigation/Navigation";
 import Article from "./Article";
+
+import contentsEN from "components/UI/Navigation/map-en";
+import contentsRU from "components/UI/Navigation/map-ru";
+
 import style from "./layout-article.module.css";
 
-const LayoutArticle = ({ children }) => (
-  <main className={style["layout-article"]}>
-    <Article>
-      {children}
-    </Article>
-  </main>
-);
+/* const contents = async ({ lang }) => {
+  return await {
+    "EN": import("components/UI/Navigation/map-en"),
+    "RU": import("components/UI/Navigation/map-ru")
+  }[lang];
+}; */
+
+const LayoutArticle = ({ children }) => {
+
+  return (
+    <main className={style["layout-article"]}>
+      <Navigation items={contentsRU} />
+      <Article>
+        {children}
+      </Article>
+    </main>
+  );
+};
 
 export default LayoutArticle;
