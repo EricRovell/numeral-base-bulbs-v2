aeb04b9f176e82393d66de2c6b214c1ef4c5e3fc
Eric Rovell <ericrovell@gmail.com>
2020-02-27 22:04:29 +0300

    tutorial contents data, en

---
+++ src/components/UI/Navigation/map-en.js
@@ -0,0 +1,49 @@
+export default [
+  {
+    label: "Contents",
+    nodes: [
+
+      {
+        label: "Bases",
+        nodes: [
+          {
+            label: "Whats is numeral base?",
+            href: "/tutorial/ru/bases",
+          },
+          {
+            label: "Non-positional numeral bases",
+            href: "/tutorial/ru/bases/non-positional",
+          },
+          {
+            label: "Positional numeral bases",
+            href: "/tutorial/ru/bases/positional",
+          },
+        ]
+      },
+
+      {
+        label: "Sandbox",
+        nodes: [
+          
+          {
+            label: "Sandbox Mode, what is it?",
+            href: "/tutorial/ru/sandbox",
+          },
+
+          {
+            label: "Symbols",
+            href: "/tutorial/ru/sandbox/symbols",
+          },
+
+          {
+            label: "Number",
+            href: "/tutorial/ru/sandbox/number",
+          },
+
+        ]
+      },
+
+    ],
+  },
+];
+
