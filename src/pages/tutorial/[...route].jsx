2d49bd282a0fd4cb726e89e01848ca3b228c34ae
Eric Rovell <ericrovell@gmail.com>
2020-03-05 11:47:59 +0300

    catching all routes and loading contents dynamically

--- src/pages/tutorial/[...route].jsx
+++ src/pages/tutorial/[...route].jsx
@@ -1,24 +1,40 @@
-import { useRouter } from "next/router";
+import dynamic from "next/dynamic";
 
 import LayoutArticle from "components/Layout/Article/LayoutArticle";
-import tutorialMap from "tutorial/tutorial-map";
+import Loader from "components/Loader/Loader";
 import Error from "pages/_error";
 
-
-const Tutorial = () => {
-
-  const { query: { route }} = useRouter();
-  const path = route && route.join("/");
-
-  if (route && tutorialMap()[path] === "undefined") {
-    return Error;
-  }
-
-  return (
-    <LayoutArticle>
-      {route && tutorialMap()[path].mdx}
-    </LayoutArticle>
+const Articles = ({ route }) => {
+  // dynamically import mdx components from "tutorial/..."
+  const Article =  dynamic(
+    () => import(`tutorial/${route}.mdx`).catch(err => {
+      return () => <Error />
+    }),
+    { loading: () => <Loader /> }
   );
+  return <Article />
+};
+
+const Tutorial = ({ route, contents }) => (
+  <LayoutArticle contents={contents}>      
+    <Articles route={route} />
+  </LayoutArticle>
+);
+
+Tutorial.getInitialProps = async ({ query: { route }}) => {
+  // if lang will be needed as prop, it should be upperCase
+  //const lang = route[0].toUpperCase();
+
+  // dynamically importing contents data in required language
+  const lang = route[0];
+  const contents = (await import(
+    `components/UI/Navigation/map-${lang}`
+  )).default;
+  
+  return {
+    route: route.join("/"),
+    contents
+  };
 };
 
 export default Tutorial;
