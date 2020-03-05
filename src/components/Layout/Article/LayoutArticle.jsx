a1667b4b7d94bef58b23c9d79da0d8e7e9eb7218
Eric Rovell <ericrovell@gmail.com>
2020-03-05 11:43:39 +0300

    getting contents from props

--- src/components/Layout/Article/LayoutArticle.jsx
+++ src/components/Layout/Article/LayoutArticle.jsx
@@ -1,28 +1,15 @@
 import Navigation from "components/UI/Navigation/Navigation";
 import Article from "./Article";
 
-import contentsEN from "components/UI/Navigation/map-en";
-import contentsRU from "components/UI/Navigation/map-ru";
-
 import style from "./layout-article.module.css";
 
-/* const contents = async ({ lang }) => {
-  return await {
-    "EN": import("components/UI/Navigation/map-en"),
-    "RU": import("components/UI/Navigation/map-ru")
-  }[lang];
-}; */
-
-const LayoutArticle = ({ children }) => {
-
-  return (
-    <main className={style["layout-article"]}>
-      <Navigation items={contentsRU} />
-      <Article>
-        {children}
-      </Article>
-    </main>
-  );
-};
+const LayoutArticle = ({ children, contents }) => (
+  <main className={style["layout-article"]}>
+    <Navigation items={contents} />
+    <Article>
+      {children}
+    </Article>
+  </main>
+);
 
 export default LayoutArticle;
