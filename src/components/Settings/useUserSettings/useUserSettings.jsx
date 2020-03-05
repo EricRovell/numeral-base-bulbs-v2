12d4d7b1711a12ec3e0080dedf75bf54d90c191c
Eric Rovell <ericrovell@gmail.com>
2020-03-05 11:44:16 +0300

    some comments have been added

--- src/components/Settings/useUserSettings/useUserSettings.jsx
+++ src/components/Settings/useUserSettings/useUserSettings.jsx
@@ -1,7 +1,13 @@
 import { useState, useEffect } from "react";
 
 export default function useUserSettings(state, item) {
-  // item: localStorage item key, string
+  /**
+    * Return custom state from localStrorage if present, overriding the default state
+    * @param {object} state Default state
+    * @param {string} item  Local Storage key to get custom settings
+    * 
+    * @return {object} Array with combined state and loading indicator
+  */
   const [ isLoading, setIsLoading ] = useState(true);
   const [ storageState, setStorageState ] = useState(null);
 
