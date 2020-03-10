48908fd999494fba5245102b79bb4b744df3d4bb
Eric Rovell <ericrovell@gmail.com>
2020-03-10 12:13:04 +0300

    using loader

--- src/pages/game.jsx
+++ src/pages/game.jsx
@@ -1,6 +1,15 @@
-const GamePage = () => {
+import { useStateContext } from "components/StateProvider/Context"; 
+import Loading from "components/Loader/Loading/Loading";
+import style from "style/pages/game.module.css";
 
-  return null;
+const GamePage = () => {
+  const [ { lang } ] = useStateContext(); 
+
+  return (
+    <main className={style.game}>
+      <Loading lang={lang} />
+    </main>
+  );
 };
 
 export default GamePage;
