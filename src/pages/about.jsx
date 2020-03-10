4b676c03ff2e3d3839ea87ce48c76a8a1c3d552d
Eric Rovell <ericrovell@gmail.com>
2020-03-10 12:12:51 +0300

    dynamic article load

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,11 +1,25 @@
+import { useRouter } from "next/router";
+import dynamic from "next/dynamic";
+
 import LayoutArticle from "components/Layout/Article/LayoutArticle";
-import AboutRU from "tutorial/about.mdx";
+
+const AboutArticle = ({ lang }) => {
+  const Article =  dynamic(
+    () => import(`tutorial/about-${lang}.mdx`).catch(err => {
+      return () => <Error />
+    })
+  );
+  return <Article />
+};
 
 const AboutPage = () => {
+
+  const { asPath } = useRouter();
+  const lang = asPath.split("/")[2];
   
   return (
     <LayoutArticle>
-      <AboutRU />
+      <AboutArticle lang={lang} />
     </LayoutArticle>
   );
 };
