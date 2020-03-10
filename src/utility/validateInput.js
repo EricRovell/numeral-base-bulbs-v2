7461a82195b29c841a4357c240c34ce678edee5d
Eric Rovell <ericrovell@gmail.com>
2020-03-11 02:45:22 +0300

    fixed validation regex bcs of escape char

--- src/utility/validateInput.js
+++ src/utility/validateInput.js
@@ -2,13 +2,13 @@ export const baseRegExp = base => {
   switch (true) {
     case (base <= 10 && base >= 2):
       return new RegExp(`^[0-${base - 1}]+$`);
-    case (base <= 36 && base > 11):
+    case (base <= 36 && base >= 11):
       const char = String.fromCharCode(base + 54);
-      return new RegExp(`^[\dA-${char}]+$`);
+      return new RegExp(`^[\\dA-${char}]+$`);
     case (base === 11):
-      return /[\dA]+/;  
+      return /[\\dA]+/;  
     case (base === 1):
     default:
-      return /[\d]+/;
+      return /[\\d]+/;
   }  
 };
