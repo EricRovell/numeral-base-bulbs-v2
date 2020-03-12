e90f7fe70a2d9d457e221d251512409abf84bdd0
Eric Rovell <ericrovell@gmail.com>
2020-03-12 14:18:50 +0300

    allowed input for lowerCase letters

--- src/utility/validateInput.js
+++ src/utility/validateInput.js
@@ -4,9 +4,9 @@ export const baseRegExp = base => {
       return new RegExp(`^[0-${base - 1}]+$`);
     case (base <= 36 && base >= 11):
       const char = String.fromCharCode(base + 54);
-      return new RegExp(`^[\\dA-${char}]+$`);
+      return new RegExp(`^[\\dA-${char}a-${char.toLowerCase()}]+$`);
     case (base === 11):
-      return /[\\dA]+/;  
+      return /[\\dAa]+/;  
     case (base === 1):
     default:
       return /[\\d]+/;
