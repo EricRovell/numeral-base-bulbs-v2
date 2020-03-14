4ed899dc6a488adbb89538bf4cde68fd97344cd3
Eric Rovell <ericrovell@gmail.com>
2020-03-14 11:45:48 +0300

    removed wrapper

--- src/components/Mode/Sandbox/Settings/Labels/Labels.jsx
+++ src/components/Mode/Sandbox/Settings/Labels/Labels.jsx
@@ -1,24 +1,19 @@
-import SettingsSection from "components/Settings/Section";
-
 import { labelsUp, labelsDown } from "./data";
 import Select from "components/Settings/Controls/Select";
 
-const LabelsForm = ({ valueUp, valueDown, handleInputChange }) => {
-
-  return (
-    <SettingsSection name={"Labels"}>
-      <Select
-        name={"labelsUp"}
-        data={labelsUp}
-        value={valueUp}
-        handleInputChange={handleInputChange} />
-      <Select
-        name={"labelsDown"}
-        data={labelsDown}
-        value={valueDown}
-        handleInputChange={handleInputChange} />
-    </SettingsSection>
-  );
-};
+const LabelsForm = ({ valueUp, valueDown, handleInputChange }) => (
+  <>  
+    <Select
+      name={"labelsUp"}
+      data={labelsUp}
+      value={valueUp}
+      handleInputChange={handleInputChange} />
+    <Select
+      name={"labelsDown"}
+      data={labelsDown}
+      value={valueDown}
+      handleInputChange={handleInputChange} />
+  </>
+);
 
 export default LabelsForm;
