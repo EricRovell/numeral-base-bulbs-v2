398bdfb3e2267c643a01c1b09af0de459d17fdaa
Eric Rovell <ericrovell@gmail.com>
2020-03-14 11:49:24 +0300

    using validation; disabled state; translations

--- src/components/Settings/BaseDigits/Shifter.jsx
+++ src/components/Settings/BaseDigits/Shifter.jsx
@@ -1,42 +1,81 @@
+import { useModalContext } from "components/UI/Modal/context";
 import { useStateContext } from "components/StateProvider/Context";
 
 import ShifterIcon from "./ShifterIcon";
-import translation from "./translation";
+import validate from "./validate";
+import translation from "./translation-ru";
+
+/* const translationsData = async ({ lang = "en", digitsMin, digitsMax }) => {
+  const data = (await import(`./translation-${lang}`)).default;
+  return data;
+}; */
 
 const Shifter = ({ increment, digits, digitsMin, digitsMax, dispatch }) => {
 
   const [ { lang } ] = useStateContext();
+  const [ _, dispatchModal ] = useModalContext();
+
+  /* const translation = translationsData({
+    lang: lang.toLowerCase() || "en",
+    digitsMin,
+    digitsMax
+  }); */
 
   const addDigits = () => {
-    // prevent adding more digits than allowed
-    if (increment === 1) {
-      if (digits.length >= digitsMax) {
-        alert(translation.maxDigitsAlert[lang]);
-        return;
-      }
-    }
-
-    // prevent removing more digits than allowed
-    if (increment === -1) {
-      if (digits.length <= digitsMin) {
-        alert(translation.minDigitsAlert[lang]);
-        return;
-      }
-    }
-
-    dispatch({
-      type: "addDigit",
-      value: increment,
+    const result = validate({
+      increment,
+      digitsLength: digits.length,
+      digitsMin,
+      digitsMax
     });
-  };
+
+    switch(result) {
+      case 0:
+        dispatch({
+          type: "addDigit",
+          value: increment,
+        });
+        break;
+
+      case 1:
+        dispatchModal({
+          type: "show",
+          title: "Maximum digits exceeded",
+          contents: (
+            <div>
+              <p>
+                {translation.modal.maxDigits.message}
+              </p>
+            </div>  
+          )
+        });
+        break;
+
+      case -1:
+        dispatchModal({
+          type: "show",
+          title: "Minimum digits exceeded",
+          contents: (
+            <span>{translation.modal.minDigits.message}</span>
+          )
+        });
+        break;
+      
+      default:
+        return;
+    }
+  }
   
   return (
     <div
       onClick={addDigits}
       title={(increment == 1)
-        ? translation.addDigit[lang]
-        : translation.removeDigit[lang]}>
-      <ShifterIcon />  
+        ? translation.tip.addDigit
+        : translation.tip.removeDigit}>
+      <ShifterIcon disabled={
+        (digits.length >= digitsMax && increment === 1 ||
+         digits.length <= digitsMin && increment === -1)
+      } />
     </div>
   );
 };
