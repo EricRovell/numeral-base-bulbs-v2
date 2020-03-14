4f61dd18447460226090c163bb21890429f1ecef
Eric Rovell <ericrovell@gmail.com>
2020-03-14 11:45:52 +0300

    removed wrapper

--- src/components/Mode/Sandbox/Settings/Representation/Representation.jsx
+++ src/components/Mode/Sandbox/Settings/Representation/Representation.jsx
@@ -1,25 +1,20 @@
-import SettingsSection from "components/Settings/Section";
-
 import { data } from "./data";
 import RadioWithOptions from "components/Settings/Controls/RadioWithOptions";
 
-const RepresentationForm = ({ skin, mode, handleInputChange }) => {
-
-  return (
-    <SettingsSection name={"Representation"}>
-      {data.map(modeRepr => (
-        <RadioWithOptions 
-          key={modeRepr.representation}
-          data={modeRepr}
-          nameRadio={"mode"}
-          nameSelect={"skin"}
-          valueRadio={mode}
-          valueSelect={skin}
-          handleInputChange={handleInputChange}  
-        />
-      ))} 
-    </SettingsSection>
-  );
-};
+const RepresentationForm = ({ skin, mode, handleInputChange }) => (
+  <>
+    {data.map(modeRepr => (
+      <RadioWithOptions 
+        key={modeRepr.representation}
+        data={modeRepr}
+        nameRadio={"mode"}
+        nameSelect={"skin"}
+        valueRadio={mode}
+        valueSelect={skin}
+        handleInputChange={handleInputChange}  
+      />
+    ))} 
+  </>
+);
 
 export default RepresentationForm;
