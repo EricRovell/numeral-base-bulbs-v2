89da08d78f6858b8a79f6be9f00f5fae99fd1516
Eric Rovell <ericrovell@gmail.com>
2020-03-14 11:47:32 +0300

    Modal close button

---
+++ src/components/UI/Modal/CloseButton.jsx
@@ -0,0 +1,9 @@
+export default ({ onCLick }) => (
+  <svg onClick={onCLick} viewBox="0 0 512 512">
+    <ellipse style={{ fill: "#E04F5F" }} cx="256" cy="256" rx="256" ry="255.832"/>
+    <g transform="matrix(-0.7071 0.7071 -0.7071 -0.7071 77.26 32)">
+      <rect x="3.98" y="-427.615" style={{ fill: "#FFFFFF" }} width="55.992" height="285.672"/>
+      <rect x="-110.828" y="-312.815" style={{ fill: "#FFFFFF" }} width="285.672" height="55.992"/>
+    </g>
+  </svg>
+);
