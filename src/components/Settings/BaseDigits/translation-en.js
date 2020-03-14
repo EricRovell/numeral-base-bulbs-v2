da404268ee4080c95f2f4684f056b493e628a585
Eric Rovell <ericrovell@gmail.com>
2020-03-14 11:48:50 +0300

    translations for shifter, wip

---
+++ src/components/Settings/BaseDigits/translation-en.js
@@ -0,0 +1,16 @@
+export default {
+  modal: {
+    maxDigits: {
+      title: "Maximum Digits Exceeded",
+      message: "Sorry, the limit has been reached for the number of digits."
+    },
+    minDigits: {
+      title: "Minimum Digits Exceeded",
+      message: "Sorry, there are no digits to be removed."
+    }
+  },
+  tip: {
+    addDigit: "Click to add another digit!",
+    removeDigit: "Убрать цифру!"
+  }
+};
---
+++ src/components/Settings/BaseDigits/translation-ru.js
@@ -0,0 +1,36 @@
+/* export default {
+  maxDigitsAlert: {
+    "EN": "Sorry, the limit has been reached for the number of digits.",
+    "RU": "К сожалению, исчерпан лимит количества символов."
+  },
+  minDigitsAlert: {
+    "EN": "Sorry, there are no digits to be removed.",
+    "RU": "К сожалению, больше нет цифр."
+  },
+  addDigit: {
+    "EN": "Add a digit!",
+    "RU": "Добавить цифру!"
+  },
+  removeDigit: {
+    "EN": "Remove a digit!",
+    "RU": "Убрать цифру!"
+  }
+}; */
+
+export default {
+  modal: {
+    maxDigits: {
+      title: "Превышено максимальное количество символов",
+      message: "Sorry, the limit has been reached for the number of digits."
+    },
+    minDigits: {
+      title: "Превышено минимальное количество символов",
+      message: "Sorry, there are no digits to be removed."
+    }
+  },
+  tip: {
+    addDigit: "Добавить цифру!",
+    removeDigit: "Убрать цифру!"
+  }
+};
+
