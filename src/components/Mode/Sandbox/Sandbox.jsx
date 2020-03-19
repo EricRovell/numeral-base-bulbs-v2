118d930057d0ee15e5614a7c6b76184fab190511
Eric Rovell <ericrovell@gmail.com>
2020-03-19 15:29:45 +0300

    using ExpressOptions; using SVGLink

--- src/components/Mode/Sandbox/Sandbox.jsx
+++ src/components/Mode/Sandbox/Sandbox.jsx
@@ -2,7 +2,9 @@ import LayoutMain from "components/Layout/Main/LayoutMain";
 import DigitsSection from "./Sections/DigitsSection";
 import NumberSection from "./Sections/NumberSection";
 
+import ExpressOptions from "components/UI/ExpressOptions/ExpressOptions";
 import ExpressSettings from "components/Settings/ExpressSettings/ExpressSettings";
+import ExpressTutorial from "components/UI/SVGLink/SVGLink";
 
 import { useStateSandbox } from "components/Mode/Sandbox/State/useStateSandbox"; 
 
@@ -18,7 +20,10 @@ const Sandbox = () => {
       <NumberSection
         state={state}
         dispatch={dispatch} />
-      <ExpressSettings dispatchSB={dispatch} />        
+      <ExpressOptions>
+        <ExpressSettings dispatchSB={dispatch} />
+        <ExpressTutorial /> 
+      </ExpressOptions>       
     </LayoutMain>
   );
 };
