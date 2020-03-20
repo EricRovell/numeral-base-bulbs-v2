66db0d24bfa8b0e12825d66b6c70c341d854f0de
Eric Rovell <ericrovell@gmail.com>
2020-03-20 15:32:51 +0300

    getting translation from server

--- src/pages/settings.jsx
+++ src/pages/settings.jsx
@@ -8,25 +8,8 @@ import LayoutSettings from "components/Layout/Settings/LayoutSettings";
 import style from "style/pages/settings.module.css";
 import styleTabs from "components/Tabs/tabs-settings.module.css";
 
-const routes = {
-  name: "settings-tabs",
-  options: [
-    {
-      title: "Globals",
-      value: "globals"
-    },
-    {
-      title: "Sandbox",
-      value: "sandbox"
-    },
-    {
-      title: "Game",
-      value: "game"
-    }
-  ]
-};
 
-const SettingsSections = ({ section, lang }) => {
+const SettingsSections = ({ section, langData }) => {
   const Section = ({
     "sandbox": dynamic(() => import("components/Mode/Sandbox/Settings/Settings")),
     "globals": dynamic(() => import("components/StateProvider/SettingsGlobal/SettingsGlobal")),
@@ -34,25 +17,39 @@ const SettingsSections = ({ section, lang }) => {
   }[section]);
   
   return (
-    <Section lang={lang} />
+    <Section langData={langData} />
   );
 };
 
-const SettingsPage = () => {
+const SettingsPage = ({ langData }) => {
 
   const [ section, setSection ] = useState("sandbox");
   
   return (
     <LayoutSettings className={style.settings}>
       <Tabs
-        data={routes}
+        data={langData.sections}
         value={section}
         setTab={setSection}
         style={styleTabs["tabs-settings"]}
         lang={"EN"} />
-      <SettingsSections lang="EN" section={section} />
+      <SettingsSections
+        section={section}
+        langData={langData[section]} />
     </LayoutSettings>
   );
+};
+
+SettingsPage.getInitialProps = async ({ asPath }) => {
+  // dynamically importing contents data in required language
+  const lang = asPath.split("/")[2];
+  const langData = (await import(
+    `components/Settings/lang/settings-lang-data-${lang}`
+  )).default;
+  
+  return {
+    langData
+  };
 };
 
 export default SettingsPage;
