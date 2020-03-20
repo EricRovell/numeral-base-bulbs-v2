b7402600d232a7a5651a092ba085d7e30500cdc7
Eric Rovell <ericrovell@gmail.com>
2020-03-20 15:34:04 +0300

    base settings: get translation from props

--- src/components/Mode/Sandbox/Settings/Sections/Base.jsx
+++ src/components/Mode/Sandbox/Settings/Sections/Base.jsx
@@ -1,18 +1,23 @@
-import { dataBaseIn, dataBaseOut } from "./data";
 import NumberSelect from "components/Settings/Controls/Number";
 
-const BaseForm = ({ baseIn, baseOut, handleInputChange }) => {
+const BaseForm = ({ langData, baseIn, baseOut, handleInputChange }) => {
+
+  const data = {
+    min: 2,
+    max: 36,
+    datalist: langData.bases
+  };
 
   return (
     <>
       <NumberSelect
-        data={dataBaseIn}
+        data={{ ...data, label: langData.baseIn }}
         value={baseIn}
         name={"baseIn"}
         id={"settings-sandbox-baseIn"}
         handleInputChange={handleInputChange} />
       <NumberSelect
-        data={dataBaseOut}
+        data={{ ...data, label: langData.baseOut }}
         value={baseOut}
         name={"baseOut"}
         id={"settings-sandbox-baseOut"}
