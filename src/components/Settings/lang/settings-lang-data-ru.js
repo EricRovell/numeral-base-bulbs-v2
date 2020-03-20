4d098816d4c85cef5b5ccb2ef4b76842b6af01ff
Eric Rovell <ericrovell@gmail.com>
2020-03-20 15:31:47 +0300

    settings localization

---
+++ src/components/Settings/lang/settings-lang-data-ru.js
@@ -0,0 +1,83 @@
+export default {
+
+  sections: {
+    name: "settings-sections",
+    options: [
+      {
+        title: "Пользователь",
+        value: "globals"
+      },
+      {
+        title: "Песочница",
+        value: "sandbox"
+      },
+      {
+        title: "Испытания",
+        value: "game"
+      }
+    ]
+  },
+
+  sandbox: {
+
+    digits: {
+      name: "Символы",
+      options: {
+        min: "Минимум символов",
+        max: "Максимум символов",
+        digits: "Значения символов"
+      }
+    },
+
+    base: {
+      name: "Система счисления",
+      options: {
+        baseIn: "Символы",
+        baseOut: "Число",
+        bases: {
+          2: "Двоичная",
+          3: "Тернарная",
+          8: "Восьмериная",
+          10: "Десятеричная",
+          16: "Шестнадцатиричная"
+        }
+      }
+    },
+
+    representation: {
+      name: "Режим и внешний вид",
+      options: [
+        {
+          name: "Двоичная",
+          representation: "bulb",
+          skins: {
+            default: "по умолчанию",
+            modern: "реализм",
+            lamp: "свисающая лампа"
+          },
+        },
+        {
+          name: "Символ",
+          representation: "symbol",
+          skins: {
+            default: "по умолчанию",
+            basic: "минимализм",
+          },
+        },
+      ]
+    },
+
+    labels: {
+      name: "Подписи",
+      labelsUp: "Верхние",
+      labelsDown: "Нижние",
+      options: {
+        index: "Индекс",
+        value: "Значение",
+        power: "Степень",
+        none: "Отсутствует"
+      }
+    }
+  }
+
+};
