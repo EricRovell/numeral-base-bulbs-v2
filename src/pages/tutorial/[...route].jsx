e6fe13fad4e03b2ab4a1b1830dbcb2186eafbda0
Eric Rovell <ericrovell@gmail.com>
2020-04-02 13:02:18 +0300

    removed old code

--- src/pages/tutorial/[...route].jsx
+++ src/pages/tutorial/[...route].jsx
@@ -38,33 +38,3 @@ Tutorial.getInitialProps = async ({ query: { route }}) => {
 };
 
 export default Tutorial;
-
-/* import { useRouter } from "next/router";
-import dynamic from "next/dynamic";
-
-import LayoutArticle from "components/Layout/Article/LayoutArticle";
-
-const AboutArticle = ({ lang }) => {
-  const Article =  dynamic(
-    () => import(`tutorial/about-${lang}.mdx`).catch(err => {
-      return () => <Error />
-    })
-  );
-  return <Article />
-};
-
-const AboutPage = () => {
-
-  const { asPath } = useRouter();
-  const lang = asPath.split("/")[2];
-  
-  return (
-    <LayoutArticle>
-      <AboutArticle lang={lang} />
-    </LayoutArticle>
-  );
-};
-
-export default AboutPage; */
-
-
