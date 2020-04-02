b55bc96ec10314269168ac69a930f3d9399b4798
Eric Rovell <ericrovell@gmail.com>
2020-04-02 13:32:54 +0300

    using App Component; updated components structure

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,44 +1,11 @@
-import { StateProvider, initialState, reducer } from "components/StateProvider/Context";
-import ModuleProvider from "components/UI/Modal/context";
-import { Helmet, HelmetProvider } from "react-helmet-async";
-
-import { useStateContext } from "components/StateProvider/Context";
-
-import Header from "components/Header/Header";
-import Modal from "components/UI/Modal/Modal";
+import MyApp from "components/App/App";
 import "style/global.css";
 
-import style from "style/pages/app.module.css";
-
-const Head = () => {
-
-  const [ { lang, theme } ] = useStateContext();
-  
-  return (
-    <Helmet>
-      <html lang={lang || "EN"} theme={theme || "dark"} />
-      <title>Numeral Base Bulbs 2</title>
-    </Helmet>
-  );
-};
-
-const MyApp = ({ Component, pageProps }) => {
-
-  return (
-    <HelmetProvider>
-      <StateProvider {...{ initialState, reducer }}>
-        <ModuleProvider>
-          <div className={style.app}>
-            <Head />
-            <Header />
-            <Component {...pageProps} />
-            <Modal />
-          </div>
-        </ModuleProvider>
-      </StateProvider>
-    </HelmetProvider>
-  )
-};
+export default ({ Component, pageProps }) => (
+  <MyApp>
+    <Component {...pageProps} />
+  </MyApp>
+);
 
 // Only uncomment this method if you have blocking data requirements for
 // every single page in your application. This disables the ability to
@@ -52,4 +19,3 @@ const MyApp = ({ Component, pageProps }) => {
 //   return { ...appProps }
 // }
 
-export default MyApp;
