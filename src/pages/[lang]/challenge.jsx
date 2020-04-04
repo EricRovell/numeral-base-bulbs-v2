de187d321fb134e47b4665b62b86eabebb6eeb7d
Eric Rovell <ericrovell@gmail.com>
2020-04-04 13:22:50 +0300

    moved page to [lang] subpath; renamed to challenge

--- src/pages/[lang]/challenge.jsx
+++ src/pages/[lang]/challenge.jsx
@@ -2,14 +2,22 @@ import { useStateContext } from "components/StateProvider/Context";
 import Loading from "components/Loader/Loading/Loading";
 import style from "style/pages/game.module.css";
 
-const GamePage = () => {
-  const [ { lang } ] = useStateContext(); 
+const GamePage = ({ lang }) => {
+  //const [ { lang } ] = useStateContext();
+   
 
   return (
     <main className={style.game}>
       <Loading lang={lang} />
     </main>
   );
+};
+
+GamePage.getInitialProps = async ({ query: { lang }}) => {
+  console.log(lang);
+  return {
+    lang: lang.toUpperCase()
+  };
 };
 
 export default GamePage;
