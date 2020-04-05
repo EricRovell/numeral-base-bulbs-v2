71f163d7196783a9b94041973cfb8f32e5a12902
Eric Rovell <ericrovell@gmail.com>
2020-04-05 13:28:40 +0300

    header locale data files

---
+++ src/components/Header/locale/header-locale-en.js
@@ -0,0 +1,15 @@
+export default {
+  title: "Numbers",
+  routes: {
+    lang: "en",
+    sandbox: "Sandbox",
+    challenge: "Challenge",
+    tutorial: "Tutorial",
+    settings: "Settings",
+    about: "About"
+  },
+  helpers: {
+    theme: "Switch a theme",
+    lang: "Set language"
+  } 
+};
