fcfd98c102119accdcdef62d8d6ba1afeed8cc6a
Eric Rovell <ericrovell@gmail.com>
2020-04-07 12:51:04 +0300

    making ErrorPage static, wip

--- src/pages/_error.jsx
+++ src/pages/_error.jsx
@@ -1,16 +1,7 @@
-function Error({ statusCode }) {
+export default function Error() {
   return (
     <p>
-      {statusCode
-        ? `An error ${statusCode} occurred on server`
-        : 'An error occurred on client'}
+      Something bad had happened...
     </p>
   )
 }
-
-Error.getInitialProps = ({ res, err }) => {
-  const statusCode = res ? res.statusCode : err ? err.statusCode : 404
-  return { statusCode }
-}
-
-export default Error;
