fc0f94670c1712557f7c0db24a90204ef19ee04e
Eric Rovell <ericrovell@gmail.com>
2020-04-07 12:47:24 +0300

    disabled pwa temporary; locale alias

--- next.config.js
+++ next.config.js
@@ -1,4 +1,4 @@
-const path = require("path");
+/* const path = require("path");
 const withPWA = require("next-pwa");
 const withMDX = require("@next/mdx")({ extension: /\.mdx?$/ });
 
@@ -16,3 +16,22 @@ module.exports = withPWA(withMDX({
     return config;
   },
 }));
+ */
+
+const path = require("path");
+const withMDX = require("@next/mdx")({ extension: /\.mdx?$/ });
+
+module.exports = withMDX({
+  pageExtensions: ["js", "jsx", "md", "mdx"],
+
+  webpack: (config, options) => {
+    config.resolve.alias["components"] = path.join(__dirname, "src/components");
+    config.resolve.alias["style"] = path.join(__dirname, "src/style");
+    config.resolve.alias["pages"] = path.join(__dirname, "src/pages");
+    config.resolve.alias["utility"] = path.join(__dirname, "src/utility");
+    config.resolve.alias["articles"] = path.join(__dirname, "src/articles");
+    config.resolve.alias["locale"] = path.join(__dirname, "src/locale");
+
+    return config;
+  },
+});
