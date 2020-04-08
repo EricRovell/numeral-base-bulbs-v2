af0876c3d12fdc42a5a936115370e4510cf5638a
Eric Rovell <ericrovell@gmail.com>
2020-04-08 23:10:42 +0300

    sandbox-app locale data, en && ru

---
+++ src/locale/sandbox/sandbox-app-en.js
@@ -0,0 +1,26 @@
+export default {
+  digits: {
+    label: {
+      index: "Index",
+      value: "Value",
+      power: "Power's value",
+      none: "Set labels...",
+    },
+    switch: "Switch the symbol's value.",
+    increment: "Increment the symbol's value.",
+    decrement: "Decrement the symbol's value.",
+    input: "Set the symbol's value.",
+  },
+  number: {
+    input: "Set the number's value.",
+    increment: "Increment the number's value.",
+    decrement: "Decrement the number's value."
+  },
+  base: {
+    input: "Set the base value.",
+    incrementBase: "Increment the base value.",
+    decrementBase: "Decrement the base value.",    
+    addSymbol: "Add a symbol.",
+    removeSymbol: "Remove a symbol."
+  }
+};
