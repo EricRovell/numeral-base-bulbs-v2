0aacfc763bf5966f2c3462eb127bfd53525bfd19
Eric Rovell <ericrovell@gmail.com>
2020-04-08 23:12:07 +0300

    passing locale props

--- src/components/Interactive/Digits.jsx
+++ src/components/Interactive/Digits.jsx
@@ -4,13 +4,14 @@ import DigitsSection from "components/Mode/Sandbox/Sections/DigitsSection";
 import defaultState from "./State/StateDigitsSection";
 import reducer from "components/Mode/Sandbox/State/reducer";
 
-export default () => {
+export default (props) => {
 
   const [ state, dispatch ] = useReducer(reducer, defaultState);
 
   return (
     <DigitsSection {...{
       state,
+      ...props,
       dispatch
     }} />
   );
