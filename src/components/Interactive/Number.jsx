8f2a7188294d86a06c12af47f0e8c8fc25f43b12
Eric Rovell <ericrovell@gmail.com>
2020-04-08 23:13:08 +0300

    passing locale props

--- src/components/Interactive/Number.jsx
+++ src/components/Interactive/Number.jsx
@@ -4,13 +4,14 @@ import NumberSection from "components/Mode/Sandbox/Sections/NumberSection";
 import defaultState from "./State/StateDigitsSection";
 import reducer from "components/Mode/Sandbox/State/reducer";
 
-export default () => {
+export default (props) => {
 
   const [ state, dispatch ] = useReducer(reducer, defaultState);
 
   return (
     <NumberSection {...{
       state,
+      ...props,
       dispatch
     }} />
   );
