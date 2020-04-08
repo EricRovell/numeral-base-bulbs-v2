b73923b11ae6143f3414133d8389ac02f084c201
Eric Rovell <ericrovell@gmail.com>
2020-04-08 23:11:29 +0300

    using useLocale

--- src/components/Mode/Sandbox/Sandbox.jsx
+++ src/components/Mode/Sandbox/Sandbox.jsx
@@ -2,22 +2,26 @@ import LayoutMain from "components/Layout/Main/LayoutMain";
 import DigitsSection from "./Sections/DigitsSection";
 import NumberSection from "./Sections/NumberSection";
 
+import { useStateSandbox } from "components/Mode/Sandbox/State/useStateSandbox";
+import useLocale from "components/Hooks/useLocale";
+
 import ExpressOptions from "components/UI/ExpressOptions/ExpressOptions";
 import ExpressSettings from "components/Settings/ExpressSettings/ExpressSettings";
 import ExpressTutorial from "components/UI/SVGLink/SVGLink";
 
-import { useStateSandbox } from "components/Mode/Sandbox/State/useStateSandbox"; 
-
-const Sandbox = () => {
+export default function Sandbox() {
 
   const [ state, dispatch ] = useStateSandbox();
+  const [ locale ] = useLocale("sandbox/sandbox-app");
 
-  return (
+  return locale && (
     <LayoutMain>        
       <DigitsSection
+        locale={locale}
         state={state}
         dispatch={dispatch} />
       <NumberSection
+        locale={locale}
         state={state}
         dispatch={dispatch} />
       <ExpressOptions>
@@ -28,6 +32,4 @@ const Sandbox = () => {
       </ExpressOptions>       
     </LayoutMain>
   );
-};
-
-export default Sandbox;
+}
