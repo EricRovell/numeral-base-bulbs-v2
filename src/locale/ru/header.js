2f5257947584dd4a13f15938e56f79e75529fa80
Eric Rovell <ericrovell@gmail.com>
2020-04-09 13:16:23 +0300

    header locale files

---
+++ src/locale/ru/header.js
@@ -0,0 +1,15 @@
+export default {
+  title: "Числа",
+  routes: {
+    lang: "ru",
+    sandbox: "Песочница",
+    challenge: "Испытания",
+    tutorial: "Инструкции",
+    settings: "Настройки",
+    about: "О приложении"
+  },
+  helpers: {
+    theme: "Сменить тему",
+    lang: "Сменить язык"
+  } 
+};
