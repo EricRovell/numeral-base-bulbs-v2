0ec4ea99fb1c06693e916853543ae878c81f053f
Eric Rovell <ericrovell@gmail.com>
2020-04-09 12:41:06 +0300

    ArticlesDynamic Component

---
+++ src/domain/Article/ArticlesDynamic.jsx
@@ -0,0 +1,15 @@
+import dynamic from "next/dynamic";
+
+import Loader from "components/Loader/Loader";
+import Error from "pages/_error";
+
+export default function ArticlesDynamic({ domain, route }) {
+  const Article = dynamic(
+    () => import(`articles/${domain}/${route}.mdx`).catch(err => {
+      return () => <Error />
+    }),
+    { loading: () => <Loader /> }
+  );
+
+  return <Article />
+};
