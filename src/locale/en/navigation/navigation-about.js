2bbcead71a85b6ff805ab3574fb6e7d17530698e
Eric Rovell <ericrovell@gmail.com>
2020-04-09 12:41:24 +0300

    navigation locale files

---
+++ src/locale/en/navigation/navigation-about.js
@@ -0,0 +1,23 @@
+export default [
+  {
+    label: "Contents",
+    nodes: [
+
+      {
+        label: "About project",
+        nodes: [
+          {
+            label: "Purpose",
+            href: "/en/about/en/welcome",
+          },
+          {
+            label: "Attributions",
+            href: "/en/about/en/attributions",
+          },
+        ]
+      },
+
+    ],
+  },
+];
+
---
+++ src/locale/ru/navigation/navigation-about.js
@@ -0,0 +1,23 @@
+export default [
+  {
+    label: "Содержание",
+    nodes: [
+
+      {
+        label: "О проекте",
+        nodes: [
+          {
+            label: "Откуда идут корни?",
+            href: "/ru/about/ru/welcome",
+          },
+          {
+            label: "Ссылки",
+            href: "/ru/about/ru/attributions",
+          },
+        ]
+      },
+
+    ],
+  },
+];
+
