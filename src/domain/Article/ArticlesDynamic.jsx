e9dd1e45d999a1a60c86082389873c55708d0085
Eric Rovell <ericrovell@gmail.com>
2020-04-09 23:32:44 +0300

    using Bricks Loader on Error

--- src/domain/Article/ArticlesDynamic.jsx
+++ src/domain/Article/ArticlesDynamic.jsx
@@ -1,12 +1,13 @@
 import dynamic from "next/dynamic";
 
 import Loader from "components/Loader/Loader";
-import Error from "pages/_error";
+import Bricks from "components/Loaders/Bricks/Bricks";
+//import Error from "pages/_error";
 
 export default function ArticlesDynamic({ domain, route }) {
   const Article = dynamic(
     () => import(`articles/${domain}/${route}.mdx`).catch(err => {
-      return () => <Error />
+      return () => <Bricks />;
     }),
     { loading: () => <Loader /> }
   );
