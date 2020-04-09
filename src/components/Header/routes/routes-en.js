7c325aab1d165b1e7d827a7b1223025ae004cf4d
Eric Rovell <ericrovell@gmail.com>
2020-04-09 13:16:49 +0300

    removed temp Header locale management

--- src/components/Header/routes/routes-en.js
+++
@@ -1,8 +0,0 @@
-export default {
-  lang: "en",
-  sandbox: "Sandbox",
-  challenge: "Challenge",
-  tutorial: "Tutorial",
-  settings: "Settings",
-  about: "About"
-};
--- src/components/Header/routes/routes-ru.js
+++
@@ -1,8 +0,0 @@
-export default {
-  lang: "ru",
-  sandbox: "Песочница",
-  challenge: "Испытания",
-  tutorial: "Инструкции",
-  settings: "Настройки",
-  about: "О приложении"
-};
