e353a3d57944d6fca114cf095d9fb0ed4b63dc7a
Eric Rovell <ericrovell@gmail.com>
2020-04-09 23:53:18 +0300

    using user prefs

--- src/components/App/Head.jsx
+++ src/components/App/Head.jsx
@@ -1,14 +1,25 @@
-import { Helmet } from "react-helmet-async";
+import { useEffect } from "react";
 import { useStateContext } from "components/StateProvider/Context";
+import { Helmet } from "react-helmet-async";
 
 export default function Head() {
 
-  const [ { lang, theme } ] = useStateContext();
+  const [ state, dispatch ] = useStateContext();
+
+  useEffect(() => {
+    const preferences = JSON.parse(
+      localStorage.getItem("SettingsGlobal")
+    );
+
+    if (preferences) {
+      dispatch({ name: "reset", state: preferences });
+    }
+  }, []);
   
   return (
     <Helmet>
-      <html lang={lang || "EN"} theme={theme || "dark"} />
+      <html lang={state.lang || "EN"} theme={state.theme || "dark"} />
       <title>Numeral Base Bulbs 2</title>
     </Helmet>
   );
-};
+}
