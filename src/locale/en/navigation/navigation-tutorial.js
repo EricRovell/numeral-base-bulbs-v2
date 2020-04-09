cf46d1aac5479616bdbc3b7c189f502964972006
Eric Rovell <ericrovell@gmail.com>
2020-04-09 12:41:38 +0300

    navigation tutorial files

---
+++ src/locale/en/navigation/navigation-tutorial.js
@@ -0,0 +1,49 @@
+export default [
+  {
+    label: "Contents",
+    nodes: [
+
+      {
+        label: "Bases",
+        nodes: [
+          {
+            label: "Whats is numeral base?",
+            href: "/en/tutorial/en/bases/numeral-bases",
+          },
+          {
+            label: "Non-positional numeral bases",
+            href: "/en/tutorial/en/bases/non-positional",
+          },
+          {
+            label: "Positional numeral bases",
+            href: "/en/tutorial/en/bases/positional",
+          },
+        ]
+      },
+
+      {
+        label: "Sandbox",
+        nodes: [
+          
+          {
+            label: "Sandbox Mode, what is it?",
+            href: "/en/tutorial/en/sandbox/about",
+          },
+
+          {
+            label: "Symbols",
+            href: "/en/tutorial/en/sandbox/symbols",
+          },
+
+          {
+            label: "Number",
+            href: "/en/tutorial/en/sandbox/number",
+          },
+
+        ]
+      },
+
+    ],
+  },
+];
+
