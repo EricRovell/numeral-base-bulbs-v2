df0773ce03907746276840aed0193b4df4b8a96e
Eric Rovell <ericrovell@gmail.com>
2020-04-10 17:36:42 +0300

    updated loader import path

--- src/pages/[lang]/sandbox.jsx
+++ src/pages/[lang]/sandbox.jsx
@@ -2,14 +2,14 @@ import useUserSettings from "components/Hooks/useUserSettings";
 import { StateProvider, defaultState, reducer } from "components/Mode/Sandbox/State/useStateSandbox";
 
 import Sandbox from "components/Mode/Sandbox/Sandbox";
-import Loader from "components/Loader/Loader";
+import LoaderPuzzle from "components/Loaders/LoaderPuzzle/LoaderPuzzle";
 
 export default function HomePage() {
 
   const [ state, isLoading ] = useUserSettings(defaultState, "SettingsSandbox");
 
   if (isLoading) {
-    return <Loader />
+    return <LoaderPuzzle />
   }
 
   return !isLoading && (
