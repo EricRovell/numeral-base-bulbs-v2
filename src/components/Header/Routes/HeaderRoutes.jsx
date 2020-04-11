29d918cb8fe2707055982a6a4ce3f4053e9ef2ff
Eric Rovell <ericrovell@gmail.com>
2020-04-11 22:42:33 +0300

    Moved nav routes to another component

---
+++ src/components/Header/Routes/HeaderRoutes.jsx
@@ -0,0 +1,21 @@
+import Link from "./Link";
+import createRoutes from "./routes";
+
+export default function Routes({ locale, handleClick, styles }) {
+  return (
+    <nav className={styles.navigation}>  
+      {createRoutes(locale).map(route => (
+        <Link
+          href={route.href}
+          as={route.as}
+          key={route.label}
+          className={styles.section}
+          activeClassName={styles["section-active"]}>
+            <a title={route.label} onClick={handleClick}>
+              {route.label}
+            </a>
+        </Link>
+      ))}
+    </nav>
+  );
+};
--- src/components/Header/Routes/Link.js
+++ src/components/Header/Routes/Link.js
@@ -4,7 +4,7 @@ import { Children } from "react";
 import Link from "next/link";
 import { withRouter } from "next/router";
 
-export default withRouter(({ router, children, as, href, className, activeClassName, ...rest }) => (
+export default withRouter(({ router, children, as, href, className, activeClassName,...rest }) => (
   <Link {...rest} href={href} as={as}>
     {React.cloneElement(Children.only(children), {
       className: (router.pathname.split("/")[2] === href.split("/")[2]) ? activeClassName : className
