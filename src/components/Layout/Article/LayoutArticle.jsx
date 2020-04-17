814c78c172f37483e4e11018fffe8a18cdc151cf
Eric Rovell <ericrovell@gmail.com>
2020-04-18 02:54:58 +0300

    using MobileMenu

--- src/components/Layout/Article/LayoutArticle.jsx
+++ src/components/Layout/Article/LayoutArticle.jsx
@@ -1,4 +1,5 @@
 import Navigation from "components/UI/Navigation/Navigation";
+import NavigationMobileToggle from "components/UI/Navigation/NavigationMobileToggle"
 import Article from "./Article";
 
 import style from "./layout-article.module.css";
@@ -6,6 +7,7 @@ import style from "./layout-article.module.css";
 const LayoutArticle = ({ children, contents, href }) => (
   <main className={style["layout-article"]}>
     <Navigation items={contents} href={href} />
+    <NavigationMobileToggle items={contents} href={href} />
     <Article>
       {children}
     </Article>
