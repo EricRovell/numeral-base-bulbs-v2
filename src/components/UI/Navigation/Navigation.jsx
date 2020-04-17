f3e7c8b91b211a0134d87422c72d0a957ba8a0ed
Eric Rovell <ericrovell@gmail.com>
2020-04-18 02:55:54 +0300

    using SubMenu

--- src/components/UI/Navigation/Navigation.jsx
+++ src/components/UI/Navigation/Navigation.jsx
@@ -1,52 +1,18 @@
-import ActiveLink from "./ActiveLinkExact";
-import Chevron from "./Chevron";
-
+import NavigationSubmenu from "./NavigationSubmenu";
 import style from "./navigation.module.css";
 
-const SubMenu = ({ items, href }) => {
-
-  if (items.nodes && items.nodes.length > 0) {
-    return (
-      <section>
-        <input type="checkbox" id={items.label} defaultChecked />
-        <label htmlFor={items.label}>
-          <Chevron />
-          <span>{items.label}</span>
-        </label>        
-        <ul>
-          {items.nodes.map(item => (
-            <li key={item.label}>
-              <SubMenu items={item} href={href} />
-            </li>
-          ))}
-        </ul>
-      </section>
-    );
-  }
-
-  return (    
-    <ActiveLink
-      href={href}
-      as={items.href}
-      className={style["navigation-link"]}
-      activeClassName={style["navigation-link-active"]}>
-        <a title={items.label}>
-          {items.label}
-        </a>
-    </ActiveLink>
-  );
-};
-
-const Navigation = ({ items, href }) => {
+const Navigation = ({ items, href, preventPropagation }) => {
 
   if (!items) return <nav />
 
   return (
     <nav className={style.navigation}>
       {items.map(item => (
-        <SubMenu
+        <NavigationSubmenu
           items={item}
           href={href}
+          style={style}
+          preventPropagation={preventPropagation}
           key={item.label} />
       ))}
     </nav>
