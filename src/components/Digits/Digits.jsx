e95ae1d1061d1e10f02ec477cac5aa5f4afea37f
Eric Rovell <ericrovell@gmail.com>
2020-04-26 23:42:16 +0300

    Digits parent component

--- src/components/Digits/Digits.jsx
+++ src/components/Digits/Digits.jsx
@@ -1,25 +1,46 @@
-import Bulbs from "./Bulb/Bulbs";
-import Symbols from "./Symbol/Symbols";
-import EmptyState from "./EmptyState/EmptyState";
+import { useMemo } from "react";
 
-import style from "./digits.module.css"
+import loadDigits from "./loadDigits";
+import Digit from "./Digit";
+import EmptyState from "./modes/empty/EmptyState";
+import style from "./digits.module.css";
 
-const renderDigits = (props) => ({
-  "bulb": <Bulbs {...props} />,
-  "symbol": <Symbols {...props} />,
-});
+export default function Digits({ mode, skin, dispatch, digits, baseIn, labelsUp, labelsDown, locale }) {
 
-const Digits = (props) => {
+  const Component = useMemo(() => loadDigits(mode, skin), [ mode, skin]);
 
-  if (!props.digits.length) {
-    return <EmptyState dispatch={props.dispatch} />;
+  if (!digits.length) {
+    return <EmptyState dispatch={dispatch} />;
   }
 
   return (
     <div className={style.wrapper}>
-      {renderDigits({ ...props })[props.mode]}
-    </div>    
-  );
-};
+      <div className={style.container}>
+        {digits.map((value, index, arr) => {
+          // the 1st element in digits array is the last digit
+          // to prevent the props change the digit's index is reversed
+          // ! and later to get the index properly to mutate -> reversed again
+          const trueIndex = arr.length - index - 1;
 
-export default Digits;
+          return (
+            <Digit
+              key={`digit-${trueIndex}`}
+              labelsUp={labelsUp}
+              labelsDown={labelsDown}
+              index={trueIndex}
+              dispatch={dispatch}
+              baseIn={baseIn}
+              locale={locale}>
+                <Component
+                  value={value}
+                  index={trueIndex}
+                  baseIn={baseIn}
+                  dispatch={dispatch}
+                  locale={locale} />
+            </Digit>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
