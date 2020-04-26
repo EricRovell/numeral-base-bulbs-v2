d4449987965b52f68aed00db63675bafc056bd20
Eric Rovell <ericrovell@gmail.com>
2020-04-26 23:48:00 +0300

    updated props and action

---
+++ src/components/Digits/modes/symbol/InputDigit.jsx
@@ -0,0 +1,45 @@
+import useValidInput from "./useValidInput";
+import style from "components/Number/number-input.module.css";
+
+export default function InputDigit({ index, value, baseIn, title, dispatch }) {
+
+  const [ wrongInput, validate ] = useValidInput({ baseIn });
+
+  const handleChange = event => {
+    const userInput = event.target.value;
+    if (!validate(userInput)) {
+      return;
+    }
+
+    dispatch({
+      type: "mutateDigit",
+      index,
+      value: (baseIn >= 2 && baseIn <= 10 )
+        ? +userInput
+        : userInput.toUpperCase().charCodeAt() - 55
+    });
+  };
+
+  const digit2char = () => (
+    (value > 9 && value <= 36)
+      ? String.fromCharCode(value + 55)
+      : value
+  );
+
+  const onFocusSelectAll = event => {
+    event.target.select();
+  };
+
+  return (
+    <input
+      type="text"
+      title={title}
+      className={(wrongInput) ? style["number-input"] : null}
+      onChange={handleChange}
+      onFocus={onFocusSelectAll}
+      value={digit2char()}
+      maxLength={1}
+      inputMode={(baseIn >= 2 && baseIn <= 10) ? "numeric" : "text"}
+    />
+  );
+};
