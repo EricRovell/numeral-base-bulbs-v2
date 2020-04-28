c726ce3ca88f28e0d2914a2283aa874cb70ef42c
Eric Rovell <ericrovell@gmail.com>
2020-04-29 02:23:45 +0300

    Checkbox atomic component

--- src/components/Settings/Controls/Checkbox.jsx
+++ src/components/Settings/Controls/Checkbox.jsx
@@ -1,15 +1,19 @@
-const Checkbox = ({ data }) => {
-
+export default function Checkbox ({ label, name, value, handleInputChange, style }) {
   return (
-    < >
-      <label>
-        <span>{data.label}</span>
-        <input
-          type="checkbox"
-          name={data.label} />
-      </label>
-    </>
+    <label className={style}>
+      <input
+        type="checkbox"
+        style={{ display: "none" }}
+        name={name}
+        value={value}
+        checked={value}
+        onChange={handleInputChange} />
+      <svg viewBox="0 0 455 453">
+        <title>{1}</title>
+        <polyline points="53 223 172 343 402 109"  />
+        <rect x="1" y="1" width="454" height="451.74" rx="75" />
+      </svg>
+      <span>{label}</span>
+    </label>
   );
-};
-
-export default Checkbox;
+}
