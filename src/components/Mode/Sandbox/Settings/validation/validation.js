338bad335222b704582cfe37fb072956ef2052f5
Eric Rovell <ericrovell@gmail.com>
2020-04-29 02:23:10 +0300

    removed labels validation

--- src/components/Mode/Sandbox/Settings/validation/validation.js
+++ src/components/Mode/Sandbox/Settings/validation/validation.js
@@ -2,7 +2,6 @@ import validateRepr from "./representation";
 import validateRadix from "./radix";
 import validateMinMaxDigits from "./minmaxDigits";
 import validateDigits from "./digits";
-import validateLabels from "./labels";
 
 export default (state) => {
   // validation order matters
@@ -12,12 +11,10 @@ export default (state) => {
     //  -> digits
     //  -> labels
 
-  return validateLabels(
-    validateDigits(
-        validateMinMaxDigits(
-          validateRadix(
-            validateRepr(state)
-        )
+  return validateDigits(
+      validateMinMaxDigits(
+        validateRadix(
+          validateRepr(state)
       )
     )
   );
