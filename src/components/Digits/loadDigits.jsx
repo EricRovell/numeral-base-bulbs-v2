b97973140162e25c5858bbb60e0c3db5851ce16e
Eric Rovell <ericrovell@gmail.com>
2020-04-29 02:26:33 +0300

    set better default mode/skin

--- src/components/Digits/loadDigits.jsx
+++ src/components/Digits/loadDigits.jsx
@@ -1,10 +1,10 @@
 import dynamic from "next/dynamic";
-import EmptyState from "./modes/empty/EmptyState";
+import SymbolDefault from "./modes/symbol/skin/Default";
 
 const capitalize = string => string.charAt(0).toUpperCase() + string.slice(1);
 
 export default (mode, skin) => (dynamic(
   () => import(`components/Digits/modes/${mode}/skin/${capitalize(skin)}.jsx`).catch(err => {
-    return () => <EmptyState />
+    return () => <SymbolDefault />
   })
 ));
