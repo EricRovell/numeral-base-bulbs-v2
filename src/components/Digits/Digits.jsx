e8a9d4c2f84cfb53f28c0101696089812f559986
Eric Rovell <ericrovell@gmail.com>
2020-04-29 02:27:26 +0300

    updated composition

--- src/components/Digits/Digits.jsx
+++ src/components/Digits/Digits.jsx
@@ -1,45 +1,33 @@
-import { useMemo } from "react";
-
-import loadDigits from "./loadDigits";
 import Digit from "./Digit";
 import EmptyState from "./modes/empty/EmptyState";
 import style from "./digits.module.css";
 
 export default function Digits({ mode, skin, dispatch, digits, baseIn, labelsUp, labelsDown, locale }) {
 
-  const Component = useMemo(() => loadDigits(mode, skin), [ mode, skin]);
-
   if (!digits.length) {
     return <EmptyState dispatch={dispatch} />;
   }
 
+  // the 1st element in digits array is the last digit
+  // to prevent the props change the digit's index is reversed
+  // ! and later to get the index properly to mutate -> reversed again
   return (
     <div className={style.wrapper}>
       <div className={style.container}>
-        {digits.map((value, index, arr) => {
-          // the 1st element in digits array is the last digit
-          // to prevent the props change the digit's index is reversed
-          // ! and later to get the index properly to mutate -> reversed again
-          const trueIndex = arr.length - index - 1;
-
-          return (
-            <Digit
-              key={`digit-${trueIndex}`}
-              labelsUp={labelsUp}
-              labelsDown={labelsDown}
-              index={trueIndex}
-              dispatch={dispatch}
-              baseIn={baseIn}
-              locale={locale}>
-                <Component
-                  value={value}
-                  index={trueIndex}
-                  baseIn={baseIn}
-                  dispatch={dispatch}
-                  locale={locale} />
-            </Digit>
-          );
-        })}
+        {digits.map((value, index, arr) => (
+          <Digit
+            key={`digit-${arr.length - index - 1}`}
+            mode={mode}
+            skin={skin}
+            labelsUp={labelsUp}
+            labelsDown={labelsDown}
+            index={arr.length - index - 1}
+            dispatch={dispatch}
+            baseIn={baseIn}
+            locale={locale}
+            value={value}
+          />
+        ))}
       </div>
     </div>
   );
