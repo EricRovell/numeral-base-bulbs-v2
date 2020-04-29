f36c1d40844f888b6e6b6d98a4237ed0da7fb3fb
Eric Rovell <ericrovell@gmail.com>
2020-04-29 13:36:04 +0300

    removed unused props

--- src/components/Settings/BaseController/BaseNumber.jsx
+++ src/components/Settings/BaseController/BaseNumber.jsx
@@ -3,37 +3,29 @@ import InputDigit from "./subcomponents/InputDigit";
 
 import style from "./base-number.module.css";
 
-export default function BaseNumber({ base, bases, baseMin, baseMax, dispatch, locale }) {
+export default function BaseNumber({ type, baseOut, baseMin, baseMax, dispatch, locale }) {
   return (
     <div className={style["base-selector"]}>
       <div>{/* I am a wrapper... */}</div>
-      <Incrementor {...{
-          actionType: -1,
-          base,
-          value: bases[base],
-          baseMin,
-          baseMax,
-          dispatch,
-          title: locale.decrementBase
-        }} />
-      <Incrementor {...{
-        actionType: 1,
-        base,
-        value: bases[base],
-        baseMin,
-        baseMax,
-        dispatch,
-        title: locale.incrementBase
-      }} />
+      <Incrementor
+        action={-1}
+        base={type}
+        value={baseOut}
+        dispatch={dispatch}
+        title={locale.decrementBase} />
+      <Incrementor
+        action={1}
+        base={type}
+        value={baseOut}
+        dispatch={dispatch}
+        title={locale.incrementBase} />
       <InputDigit
-        {...{
-          value: bases[base],
-          base,
-          baseMin,
-          baseMax,
-          dispatch,
-          title: locale.input
-        }} />
+        type={type}
+        value={baseOut}
+        baseMin={baseMin}
+        baseMax={baseMax}
+        dispatch={dispatch}
+        title={locale.input} />
     </div>
   );
 };
