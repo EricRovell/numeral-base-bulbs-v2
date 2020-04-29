e0f7fdd7eb0b3d23ebc3425e51af63ca2d402363
Eric Rovell <ericrovell@gmail.com>
2020-04-29 13:36:00 +0300

    removed unused props

--- src/components/Settings/BaseController/BaseDigits.jsx
+++ src/components/Settings/BaseController/BaseDigits.jsx
@@ -4,52 +4,37 @@ import Shifter from "./subcomponents/Shifter";
 
 import style from "./base-digits.module.css";
 
-export default function BaseDigits({ digits, digitsMin, digitsMax, base, bases, baseMin, baseMax, dispatch, locale }) {
-
+export default function BaseDigits({ type, baseIn, baseMin, baseMax, dispatch, locale }) {
   return (
     <div className={style["base-digits"]}>
       <div>{/* I am a wrapper... */}</div>
       <Shifter
-        increment={-1}
-        digits={digits}
-        digitsMin={digitsMin}
-        digitsMax={digitsMax}
+        action={-1}
         dispatch={dispatch}
         title={locale.removeSymbol} />
       <Shifter
-        increment={1}
-        digits={digits}
-        digitsMin={digitsMin}
-        digitsMax={digitsMax}
+        action={1}
         dispatch={dispatch}
         title={locale.addSymbol} />
-      <Incrementor {...{
-        actionType: -1,
-        base,
-        value: bases[base]  ,
-        baseMin,
-        baseMax,
-        dispatch,
-        title: locale.incrementBase
-      }} />
-      <Incrementor {...{
-        actionType: 1,
-        base,
-        value: bases[base],
-        baseMin,
-        baseMax,
-        dispatch,
-        title: locale.decrementBase
-      }} />
+      <Incrementor
+        action={-1}
+        base={type}
+        value={baseIn}
+        dispatch={dispatch}
+        title={locale.decrementBase} />
+      <Incrementor
+        action={1}
+        base={type}
+        value={baseIn}
+        dispatch={dispatch}
+        title={locale.incrementBase} />
       <InputDigit
-        {...{
-          value: bases[base],
-          base,
-          baseMin,
-          baseMax,
-          dispatch,
-          title: locale.input
-        }} />
+        type={type}
+        value={baseIn}
+        baseMin={baseMin}
+        baseMax={baseMax}
+        dispatch={dispatch}
+        title={locale.input} />
     </div>
   );
 };
