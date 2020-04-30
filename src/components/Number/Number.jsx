6907ba3ec704a8a2402b985a93bed66d3da49510
Eric Rovell <ericrovell@gmail.com>
2020-04-30 11:30:30 +0300

    obj prop -> props

--- src/components/Number/Number.jsx
+++ src/components/Number/Number.jsx
@@ -3,20 +3,19 @@ import style from "./number.module.css";
 
 const Number = ({ digits, digitsMax, baseIn, baseOut, dispatch, locale }) => (
   <div className={style.number}>
-    <div />
+    <div /* I am a wrapper */ />
     <button title={locale.increment} />
     <button title={locale.decrement} />
-    <NumberInput {...{
-      digits,
-      digitsMax,
-      baseIn,
-      baseOut,
-      dispatch,
-      locale: {
+    <NumberInput
+      digits={digits}
+      digitsMax={digitsMax}
+      baseIn={baseIn}
+      baseOut={baseOut}
+      dispatch={dispatch}
+      locale={{
         title: locale.input,
         placeholder: locale.placeholder
-      }
-    }} />
+      }} />
   </div>
 );
 
