dfc9abf13ac90dc4815eabe6827afeadacdeb7cb
Eric Rovell <ericrovell@gmail.com>
2020-05-10 19:10:28 +0300

    using updated navigation

--- src/components/Layout/Article/LayoutArticle.jsx
+++ src/components/Layout/Article/LayoutArticle.jsx
@@ -1,17 +1,17 @@
 import Navigation from "components/UI/Navigation/Navigation";
-import NavigationMobileToggle from "components/UI/Navigation/NavigationMobileToggle"
+/* import NavigationMobileToggle from "components/UI/Navigation/NavigationMobileToggle" */
 import Article from "./Article";
 
 import style from "./layout-article.module.css";
 
-const LayoutArticle = ({ children, contents, href }) => (
-  <main className={style["layout-article"]}>
-    <Navigation items={contents} href={href} />
-    <NavigationMobileToggle items={contents} href={href} />
-    <Article>
-      {children}
-    </Article>
-  </main>
-);
-
-export default LayoutArticle;
+export default function LayoutArticle({ children, contents, href }) {
+  return (
+    <main className={style["layout-article"]}>
+      <Navigation data={contents} href={href} />
+      {/* <NavigationMobileToggle items={contents} href={href} /> */}
+      <Article>
+        {children}
+      </Article>
+    </main>
+  );
+}
