d972815c6fe9d47747ec623e33db20d1f4071525
Eric Rovell <ericrovell@gmail.com>
2020-05-10 19:09:59 +0300

    updated navigation using details

--- src/components/UI/Navigation/Navigation.jsx
+++ src/components/UI/Navigation/Navigation.jsx
@@ -1,22 +1,30 @@
-import NavigationSubmenu from "./NavigationSubmenu";
+import NavChevron from "./Chevron";
+import NavSection from "./NavSection";
+import ActiveLink from "./ActiveLinkExact";
+
 import style from "./navigation.module.css";
 
-const Navigation = ({ items, href, preventPropagation }) => {
-
-  if (!items) return <nav />
-
+export default function Navigation({ data, href }) {
   return (
     <nav className={style.navigation}>
-      {items.map(item => (
-        <NavigationSubmenu
-          items={item}
-          href={href}
-          style={style}
-          preventPropagation={preventPropagation}
-          key={item.label} />
+      <NavChevron />
+      <header>Contents</header>
+      {data.map(section => (
+        <NavSection key={section.title} title={section.title} className={style.section}>
+          {section.contents.map(link => (
+            <ActiveLink
+              key={link.title}
+              href={href}
+              as={link.href}
+              className={style.link}
+              activeClassName={style["link-active"]}>
+                <a title={link.title}>
+                  {link.title}
+                </a>
+            </ActiveLink>
+          ))}
+        </NavSection>
       ))}
     </nav>
   );
-};
-
-export default Navigation;
+}
