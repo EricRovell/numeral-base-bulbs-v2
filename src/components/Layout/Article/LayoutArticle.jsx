0d87b111d676165b9535130733c0df89ac2a491d
Eric Rovell <ericrovell@gmail.com>
2020-05-11 01:35:07 +0300

    using mobile navigation

--- src/components/Layout/Article/LayoutArticle.jsx
+++ src/components/Layout/Article/LayoutArticle.jsx
@@ -1,5 +1,5 @@
 import Navigation from "components/UI/Navigation/Navigation";
-/* import NavigationMobileToggle from "components/UI/Navigation/NavigationMobileToggle" */
+import NavigationMobile from "components/UI/Navigation/NavigationMobile";
 import Article from "./Article";
 
 import style from "./layout-article.module.css";
@@ -7,8 +7,13 @@ import style from "./layout-article.module.css";
 export default function LayoutArticle({ children, contents, href }) {
   return (
     <main className={style["layout-article"]}>
-      <Navigation data={contents} href={href} />
-      {/* <NavigationMobileToggle items={contents} href={href} /> */}
+      <Navigation
+        data={contents}
+        href={href} />
+      <NavigationMobile
+        className={style.toggle}
+        data={contents}
+        href={href} />
       <Article>
         {children}
       </Article>
