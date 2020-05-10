df963f7ecbb3443196fe3125dee009c365c300c7
Eric Rovell <ericrovell@gmail.com>
2020-05-10 19:03:26 +0300

    merged about and tutorial into one section, updated locale

---
+++ src/locale/ru/navigation-tutorial.js
@@ -0,0 +1,103 @@
+export default [
+
+  {
+    title: "Введение",
+    contents: [
+      {
+        title: "Краткий экскурс",
+        href: "/ru/tutorial/ru/welcome",
+      }
+    ]
+  },
+
+  {
+    title: "Системы счисления",
+    contents: [
+      {
+        title: "Что же такое система счисления?",
+        href: "/ru/tutorial/ru/bases/numeral-bases",
+      },
+      {
+        title: "Непозитионные системы счисления",
+        href: "/ru/tutorial/ru/bases/non-positional",
+      },
+      {
+        title: "Позиционные системы счисления",
+        href: "/ru/tutorial/ru/bases/positional",
+      },
+    ]
+  },
+
+  {
+    title: "Режим песочницы",
+    contents: [          
+      {
+        title: "Режим песочницы, что же это?",
+        href: "/ru/tutorial/ru/sandbox/about",
+      },
+      {
+        title: "Символ",
+        href: "/ru/tutorial/ru/sandbox/symbol",
+      },
+      {
+        title: "Символы",
+        href: "/ru/tutorial/ru/sandbox/symbols",
+      },
+      {
+        title: "Число",
+        href: "/ru/tutorial/ru/sandbox/number",
+      },
+      {
+        title: "Складывая всё воедино",
+        href: "/ru/tutorial/ru/sandbox/whole-picture",
+      },
+    ]
+  },
+
+  {
+    title: "Режим испытаний",
+    contents: [
+      {
+        title: "Нас ждут испытания",
+        href: "/ru/tutorial/ru/game/about",
+      },
+    ]
+  },
+
+  {
+    title: "Пользовательские настройки",
+    contents: [
+      {
+        title: "О предпочтениях",
+        href: "/ru/tutorial/ru/settings/about",
+      },
+      {
+        title: "Собственные предпочтения",
+        href: "/ru/tutorial/ru/settings/how-to-use",
+      },
+      {
+        title: "Настройки. Режим песочницы",
+        href: "/ru/tutorial/ru/settings/sandbox",
+      },
+    ]
+  },
+
+  {
+    title: "О приложении",
+    contents: [
+      {
+        title: "О приложении",
+        href: "/ru/tutorial/ru/about/welcome",
+      },
+      {
+        title: "Планы",
+        href: "/ru/tutorial/ru/about/todo",
+      },
+      {
+        title: "Атрибуции",
+        href: "/ru/tutorial/ru/about/attributions",
+      }
+    ],
+  },
+
+];
