47b675bc098b6c619a68b16e13a9851a6c1a2336
Eric Rovell <ericrovell@gmail.com>
2020-05-11 13:53:15 +0300

    using if tree

--- src/utility/validateInput.js
+++ src/utility/validateInput.js
@@ -1,14 +1,15 @@
 export const baseRegExp = base => {
-  switch (true) {
-    case (base <= 10 && base >= 2):
-      return new RegExp(`^[0-${base - 1}]+$`);
-    case (base <= 36 && base >= 11):
-      const char = String.fromCharCode(base + 54);
-      return new RegExp(`^[\\dA-${char}a-${char.toLowerCase()}]+$`);
-    case (base === 11):
-      return /[\\dAa]+/;  
-    case (base === 1):
-    default:
-      return /[\\d]+/;
+  if (base <= 10 && base >= 2) {
+    return new RegExp(`^[0-${base - 1}]+$`);
+  }
+  if (base <= 36 && base >= 11) {
+    const char = String.fromCharCode(base + 54);
+    return new RegExp(`^[\\dA-${char}a-${char.toLowerCase()}]+$`);
+  }
+  if (base === 11) {
+    return /[\\dAa]+/;
+  }
+  if (base === 1) {
+    return /[\\d]+/;
   }
 };
