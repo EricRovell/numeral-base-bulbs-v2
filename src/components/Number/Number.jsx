67a42b7f5d7168807437ecee6fed07acddd1ea21
Eric Rovell <ericrovell@gmail.com>
2020-05-13 14:32:42 +0300

    using Incrementor

--- src/components/Number/Number.jsx
+++ src/components/Number/Number.jsx
@@ -1,22 +1,29 @@
 import NumberInput from "./NumberInput";
+import Incrementor from "./NumberIncrementor";
 import style from "./number.module.css";
 
-const Number = ({ digits, digitsMax, baseIn, baseOut, dispatch, locale }) => (
-  <div className={style.number}>
-    <div /* I am a wrapper */ />
-    <button title={locale.increment} />
-    <button title={locale.decrement} />
-    <NumberInput
-      digits={digits}
-      digitsMax={digitsMax}
-      baseIn={baseIn}
-      baseOut={baseOut}
-      dispatch={dispatch}
-      locale={{
-        title: locale.input,
-        placeholder: locale.placeholder
-      }} />
-  </div>
-);
+export default function Number({ digits, digitsMax, baseIn, baseOut, dispatch, locale }) {
 
-export default Number;
+  return (
+    <div className={style.number}>
+      <div /* I am a wrapper */ />
+      <Incrementor 
+        action={-1}
+        className={style.incrementor} 
+        dispatch={dispatch} />
+      <Incrementor 
+        className={style.incrementor} 
+        dispatch={dispatch} />
+      <NumberInput
+        digits={digits}
+        digitsMax={digitsMax}
+        baseIn={baseIn}
+        baseOut={baseOut}
+        dispatch={dispatch}
+        locale={{
+          title: locale.input,
+          placeholder: locale.placeholder
+        }} />
+    </div>
+  );
+}
