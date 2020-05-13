f7ed3ac372a7bfa84fda329c48cc1680f209fa27
Eric Rovell <ericrovell@gmail.com>
2020-05-13 19:33:02 +0300

    header updated locale, dropdown, en

--- src/locale/en/header.js
+++ src/locale/en/header.js
@@ -1,11 +1,30 @@
 export default {
   routes: {
     lang: "en",
-    sandbox: "Sandbox",
-    challenge: "Challenge",
-    manual: "Manual",
-    settings: "Settings",
-    about: "About",
+    sandbox: {
+      main: "Sandbox",
+      settings: "Settings"
+    },
+    challenge: {
+      main: "Challenge"
+    },
+    manual: {
+      main: "Manual",
+      numeralBases: "Numeral Bases",
+      sandbox: "Sandbox",
+      challenge: "Challenge",
+      settings: "Settings",
+      about: "About",
+    },
+    settings: {
+      main: "Settings",
+      globals: "Globals",
+      sandbox: "Sandbox",
+      challenge: "Challenge"
+    },
+    about: {
+      main: "About"
+    }
   },
   helpers: {
     theme: "Switch a theme",
