29b66259eb75b80f74b9ca36b700a236abedde2a
Eric Rovell <ericrovell@gmail.com>
2020-05-13 19:32:56 +0300

    header updated locale, dropdown, ru

--- src/locale/ru/header.js
+++ src/locale/ru/header.js
@@ -1,11 +1,30 @@
 export default {
   routes: {
     lang: "ru",
-    sandbox: "Песочница",
-    challenge: "Испытания",
-    manual: "Руководство",
-    settings: "Настройки",
-    about: "О приложении",
+    sandbox: {
+      main: "Песочница",
+      settings: "Настройки"
+    },
+    challenge: {
+      main: "Испытания"
+    },
+    manual: {
+      main: "Руководство",
+      numeralBases: "Системы счисления",
+      sandbox: "Песочница",
+      challenge: "Испытания",
+      settings: "Настройки",
+      about: "Дневник",
+    },
+    settings: {
+      main: "Настройки",
+      globals: "Общие",
+      sandbox: "Песочница",
+      challenge: "Испытания"
+    },
+    about: {
+      main: "О приложении"
+    }
   },
   helpers: {
     theme: "Сменить тему",
